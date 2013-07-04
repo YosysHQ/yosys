cd33db25d174727b4c2ccb0d6b455238a81a32e3
Clifford Wolf <clifford@clifford.at>
2013-07-04 21:26:49 +0200

    Improved xsthammer quartus support

--- tests/xsthammer/run-check.sh
+++ tests/xsthammer/run-check.sh
@@ -81,7 +81,7 @@ done
 	echo "read_ilang ${job}_top_techmap.il"
 	echo "sat -timeout 60 -verify-no-timeout -show a,b,y_rtl,y_syn -prove y_rtl y_syn ${job}_top_nomap"
 	echo "sat -timeout 60 -verify-no-timeout -show a,b,y_rtl,y_syn -prove y_rtl y_syn ${job}_top_techmap"
-	if [[ $job != expression_* ]]; then
+	if [[ $job != expression_* ]] && ! $use_quartus && ! $use_vivado; then
 		echo "eval -brute_force_equiv_checker ${job}_rtl_nomap   ${job}_syn_nomap"
 		echo "eval -brute_force_equiv_checker ${job}_rtl_techmap ${job}_syn_techmap"
 	fi
