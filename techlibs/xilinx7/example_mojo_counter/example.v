b934a2d20940de78c1d33ebe481e4230744392b5
Clifford Wolf <clifford@clifford.at>
2013-10-26 17:22:29 +0200

    Added another xilinx example (not funcional yet)

---
+++ techlibs/xilinx7/example_mojo_counter/example.v
@@ -0,0 +1,14 @@
+module top(clk, led_7, led_6, led_5, led_4, led_3, led_2, led_1, led_0);
+
+input clk;
+output led_7, led_6, led_5, led_4;
+output led_3, led_2, led_1, led_0;
+
+reg [31:0] counter;
+
+always @(posedge clk)
+	counter <= counter + 1;
+
+assign {led_7, led_6, led_5, led_4, led_3, led_2, led_1, led_0} = counter >> 24;
+
+endmodule
