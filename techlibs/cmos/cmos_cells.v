0b4a64ac6adbd6c61e09517c4ea98cabd8b8b9ad
Clifford Wolf <clifford@clifford.at>
2013-10-31 12:27:35 +0100

    Added DFFSR cell to techlibs/cmos/cmos_cells.lib

--- techlibs/cmos/cmos_cells.v
+++ techlibs/cmos/cmos_cells.v
@@ -21,3 +21,15 @@ always @(posedge C)
 	Q <= D;
 endmodule
 
+module DFFSR(C, D, Q, S, R);
+input C, D, S, R;
+output reg Q;
+always @(posedge C, posedge S, posedge R)
+	if (S)
+		Q <= 1'b1;
+	else if (R)
+		Q <= 1'b0;
+	else
+		Q <= D;
+endmodule
+
