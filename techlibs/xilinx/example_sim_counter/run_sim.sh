ae798d3fd5dc6bdd82083cce3994f449b829995e
Clifford Wolf <clifford@clifford.at>
2013-11-24 17:55:46 +0100

    Fixed xilinx/example_sim_counter test bench

--- techlibs/xilinx/example_sim_counter/run_sim.sh
+++ techlibs/xilinx/example_sim_counter/run_sim.sh
@@ -8,7 +8,7 @@ XILINX_DIR=/opt/Xilinx/14.5/ISE_DS/ISE
 
 iverilog -o testbench_gold counter_tb.v counter.v
 iverilog -o testbench_gate counter_tb.v testbench_synth.v \
-	$XILINX_DIR/verilog/src/{glbl,unisims/{FDRE,LUT1,LUT2,LUT3,LUT4,LUT5,LUT6}}.v
+	$XILINX_DIR/verilog/src/{glbl,unisims/{FDRE,LUT1,LUT2,LUT3,LUT4,LUT5,LUT6,BUFGP,IBUF}}.v
 
 ./testbench_gold > testbench_gold.txt
 ./testbench_gate > testbench_gate.txt
