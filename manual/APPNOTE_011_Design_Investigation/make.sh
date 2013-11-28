9595eca181d94064fa20e4746c3888f3c8091f15
Clifford Wolf <clifford@clifford.at>
2013-11-28 17:39:16 +0100

    More progress on AppNote 011

--- manual/APPNOTE_011_Design_Investigation/make.sh
+++ manual/APPNOTE_011_Design_Investigation/make.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 ../../yosys example.ys
-sed -i '/^label=/ d;' example_*.dot
+../../yosys -p 'proc; opt; show -format dot -prefix splice' splice.v
+sed -i '/^label=/ d;' example_*.dot splice.dot
 dot -Tpdf -o example_00.pdf example_00.dot
 dot -Tpdf -o example_01.pdf example_01.dot
 dot -Tpdf -o example_02.pdf example_02.dot
+dot -Tpdf -o splice.pdf splice.dot
