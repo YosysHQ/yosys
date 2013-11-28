f89ecbc10002b4749099f189d60bc591fefe9467
Clifford Wolf <clifford@clifford.at>
2013-11-28 23:09:03 +0100

    Progress on AppNote 011

---
+++ manual/APPNOTE_011_Design_Investigation/cmos.v
@@ -0,0 +1,3 @@
+module cmos_demo(input a, b, output [1:0] y);
+assign y = a + b;
+endmodule
--- manual/APPNOTE_011_Design_Investigation/make.sh
+++ manual/APPNOTE_011_Design_Investigation/make.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 ../../yosys example.ys
 ../../yosys -p 'proc; opt; show -format dot -prefix splice' splice.v
-sed -i '/^label=/ d;' example_*.dot splice.dot
+../../yosys -p 'techmap; abc -liberty ../../techlibs/cmos/cmos_cells.lib;; show -format dot -prefix cmos_00' cmos.v
+../../yosys -p 'techmap; splitnets -ports; abc -liberty ../../techlibs/cmos/cmos_cells.lib;; show -lib ../../techlibs/cmos/cmos_cells.v -format dot -prefix cmos_01' cmos.v
+sed -i '/^label=/ d;' example_*.dot splice.dot cmos_*.dot
 dot -Tpdf -o example_00.pdf example_00.dot
 dot -Tpdf -o example_01.pdf example_01.dot
 dot -Tpdf -o example_02.pdf example_02.dot
 dot -Tpdf -o splice.pdf splice.dot
+dot -Tpdf -o cmos_00.pdf cmos_00.dot
+dot -Tpdf -o cmos_01.pdf cmos_01.dot
