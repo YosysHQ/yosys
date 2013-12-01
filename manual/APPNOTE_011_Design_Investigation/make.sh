73e28f0e3900cc071f13365748212f1ef10cf0e2
Clifford Wolf <clifford@clifford.at>
2013-12-01 14:07:44 +0100

    Progress on AppNote 011

--- manual/APPNOTE_011_Design_Investigation/make.sh
+++ manual/APPNOTE_011_Design_Investigation/make.sh
@@ -5,7 +5,13 @@
 ../../yosys -p 'techmap; splitnets -ports; abc -liberty ../../techlibs/cmos/cmos_cells.lib;; show -lib ../../techlibs/cmos/cmos_cells.v -format dot -prefix cmos_01' cmos.v
 ../../yosys -p 'opt; cd sumprod; select a:sumstuff; show -format dot -prefix sumprod_00' sumprod.v
 ../../yosys -p 'opt; cd sumprod; select a:sumstuff %x; show -format dot -prefix sumprod_01' sumprod.v
-sed -i '/^label=/ d;' example_*.dot splice.dot cmos_*.dot sumprod_*.dot
+../../yosys -p 'opt; cd sumprod; select prod; show -format dot -prefix sumprod_02' sumprod.v
+../../yosys -p 'opt; cd sumprod; select prod %ci; show -format dot -prefix sumprod_03' sumprod.v
+../../yosys -p 'opt; cd sumprod; select prod %ci2; show -format dot -prefix sumprod_04' sumprod.v
+../../yosys -p 'opt; cd sumprod; select prod %ci3; show -format dot -prefix sumprod_05' sumprod.v
+../../yosys -p 'proc; opt; memory; opt; cd memdemo; show -format dot -prefix memdemo_00' memdemo.v
+../../yosys -p 'proc; opt; memory; opt; cd memdemo; show -format dot -prefix memdemo_01 y %ci2:+$dff[Q,D] %ci*:-$mux[S]:-$dff' memdemo.v
+sed -i '/^label=/ d;' example_*.dot splice.dot cmos_*.dot sumprod_*.dot memdemo_*.dot
 dot -Tpdf -o example_00.pdf example_00.dot
 dot -Tpdf -o example_01.pdf example_01.dot
 dot -Tpdf -o example_02.pdf example_02.dot
@@ -15,3 +21,9 @@ dot -Tpdf -o cmos_00.pdf cmos_00.dot
 dot -Tpdf -o cmos_01.pdf cmos_01.dot
 dot -Tpdf -o sumprod_00.pdf sumprod_00.dot
 dot -Tpdf -o sumprod_01.pdf sumprod_01.dot
+dot -Tpdf -o sumprod_02.pdf sumprod_02.dot
+dot -Tpdf -o sumprod_03.pdf sumprod_03.dot
+dot -Tpdf -o sumprod_04.pdf sumprod_04.dot
+dot -Tpdf -o sumprod_05.pdf sumprod_05.dot
+dot -Tpdf -o memdemo_00.pdf memdemo_00.dot
+dot -Tpdf -o memdemo_01.pdf memdemo_01.dot
