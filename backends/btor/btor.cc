583636f0adb49184358cb9dea3b7a6d3394e3fbf
Clifford Wolf <clifford@clifford.at>
2014-02-05 18:31:10 +0100

    Added BTOR backend README file

--- backends/btor/btor.cc
+++ backends/btor/btor.cc
@@ -960,7 +960,7 @@ struct BtorBackend : public Backend {
 				continue;
 
 			if (module->processes.size() != 0)
-				log_error("Found unmapped processes in module %s: unmapped processes are not supported in BLIF backend!\n", RTLIL::id2cstr(module->name));
+				log_error("Found unmapped processes in module %s: unmapped processes are not supported in BTOR backend!\n", RTLIL::id2cstr(module->name));
 
 			if (module->name == RTLIL::escape_id(top_module_name)) {
 				BtorDumper::dump(f, module, design, config);
