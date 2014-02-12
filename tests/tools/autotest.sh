9ce7b0fc3bcd1fcb8cebe4f3c86318eaa4fbc943
Clifford Wolf <clifford@clifford.at>
2014-02-12 13:11:58 +0100

    Disabled "abc -dff" in "make test" for now (waiting for scorr bugfix in ABC)

--- tests/tools/autotest.sh
+++ tests/tools/autotest.sh
@@ -114,7 +114,8 @@ do
 			test_passes -p "$scriptopt"
 		else
 			test_passes -p "hierarchy; proc; opt; memory; opt; fsm; opt"
-			test_passes -p "hierarchy; proc; opt; memory; opt; fsm; opt; techmap; opt; abc -dff; opt"
+			# test_passes -p "hierarchy; proc; opt; memory; opt; fsm; opt; techmap; opt; abc -dff; opt"
+			test_passes -p "hierarchy; proc; opt; memory; opt; fsm; opt; techmap; opt; abc; opt"
 		fi
 		touch ../${bn}.log
 	}
