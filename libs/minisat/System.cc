948d04c06c181a1c270017b6d7c819426a360303
Siesh1oo <siesh1oo@siesh1oo.no>
2014-03-12 14:12:52 +0100

    - libs/minisat/System.cc: fix definition/declaration mismatch for Minisat::memUsedPeak() and mark unused parameters as unused to fix compiler error+warning. (minisat bug tracker issues #1, #9, #10.)

--- libs/minisat/System.cc
+++ libs/minisat/System.cc
@@ -79,7 +79,7 @@ double Minisat::memUsed() {
     struct rusage ru;
     getrusage(RUSAGE_SELF, &ru);
     return (double)ru.ru_maxrss / 1024; }
-double Minisat::memUsedPeak() { return memUsed(); }
+double Minisat::memUsedPeak(bool strictlyPeak) { (void) strictlyPeak; return memUsed(); }
 
 
 #elif defined(__APPLE__)
@@ -89,11 +89,11 @@ double Minisat::memUsed() {
     malloc_statistics_t t;
     malloc_zone_statistics(NULL, &t);
     return (double)t.max_size_in_use / (1024*1024); }
-double Minisat::memUsedPeak() { return memUsed(); }
+double Minisat::memUsedPeak(bool strictlyPeak) { (void) strictlyPeak; return memUsed(); }
 
 #else
 double Minisat::memUsed()     { return 0; }
-double Minisat::memUsedPeak() { return 0; }
+double Minisat::memUsedPeak(bool strictlyPeak) { (void) strictlyPeak; return 0; }
 #endif
 
 
