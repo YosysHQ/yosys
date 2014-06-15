656685fa31b54cebf0210518d3cac8aa496dddd7
Clifford Wolf <clifford@clifford.at>
2014-06-15 08:48:41 +0200

    Improved realmath test bench

--- tests/realmath/run-test.sh
+++ tests/realmath/run-test.sh
@@ -13,7 +13,7 @@ for ((i = 0; i < 100; i++)); do
 	idx=$( printf "%05d" $i )
 	../../../yosys -q uut_${idx}.ys
 	iverilog -o uut_${idx}_tb uut_${idx}_tb.v uut_${idx}.v uut_${idx}_syn.v
-	./uut_${idx}_tb > uut_${idx}.log
+	./uut_${idx}_tb
 done
 echo
 
