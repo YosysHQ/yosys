4af8d84f015edd229328a09c462c8ad81e06892f
Clifford Wolf <clifford@clifford.at>
2014-07-20 17:05:20 +0200

    Small fix in tests/vloghtb/run-test.sh

--- tests/vloghtb/run-test.sh
+++ tests/vloghtb/run-test.sh
@@ -8,5 +8,7 @@ tar --strip=1 -xjf vloghammer_tb.tar.bz2
 
 make clean
 make -j4 EXIT_ON_ERROR=1 YOSYS_BIN=$PWD/../../yosys YOSYS_SCRIPT="proc;;" check_yosys
+
+rm -rf log_test_*
 make -j4 -f test_makefile MODE=share
 
