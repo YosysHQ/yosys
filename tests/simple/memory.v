cad98bcd89cd9747f3ea9e35eed8d9bbedd64d7a
Clifford Wolf <clifford@clifford.at>
2014-08-12 10:37:47 +0200

    Added multi-dim memory test (requires iverilog git head)

--- tests/simple/memory.v
+++ tests/simple/memory.v
@@ -194,3 +194,14 @@ always @(posedge clk) begin
 end
 
 endmodule
+
+// ----------------------------------------------------------
+
+module memtest08(input clk, input [3:0] a, b, c, output reg [3:0] y);
+	reg [3:0] mem [0:15] [0:15];
+	always @(posedge clk) begin
+		y <= mem[a][b];
+		mem[a][b] <= c;
+	end
+endmodule
+
