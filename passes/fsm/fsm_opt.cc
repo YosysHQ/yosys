e5b8390f44be7d300fdf32193fa58b76ba448072
William Speirs <bill.speirs@gmail.com>
2014-10-14 17:15:35 -0400

    Changed from "and" to "&&"

--- passes/fsm/fsm_opt.cc
+++ passes/fsm/fsm_opt.cc
@@ -342,7 +342,7 @@ struct FsmOptPass : public Pass {
 		for (auto &mod_it : design->modules_) {
 			if (design->selected(mod_it.second))
 				for (auto &cell_it : mod_it.second->cells_)
-					if (cell_it.second->type == "$fsm" and design->selected(mod_it.second, cell_it.second))
+					if (cell_it.second->type == "$fsm" && design->selected(mod_it.second, cell_it.second))
 						FsmData::optimize_fsm(cell_it.second, mod_it.second);
 		}
 	}
