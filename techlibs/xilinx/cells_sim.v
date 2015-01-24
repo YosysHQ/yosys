909a95182b6de03d6227f6331a6e60692d20203d
Clifford Wolf <clifford@clifford.at>
2015-01-24 11:03:22 +0100

    Fixed xilinx FDSE sim model

--- techlibs/xilinx/cells_sim.v
+++ techlibs/xilinx/cells_sim.v
@@ -119,8 +119,8 @@ module FDSE (output reg Q, input C, CE, D, S);
   parameter [0:0] IS_S_INVERTED = 1'b0;
   initial Q <= INIT;
   generate case (|IS_C_INVERTED)
-    1'b0: always @(posedge C) if (S == !IS_S_INVERTED) Q <= 1'b0; else if (CE) Q <= D ^ IS_D_INVERTED;
-    1'b1: always @(negedge C) if (S == !IS_S_INVERTED) Q <= 1'b0; else if (CE) Q <= D ^ IS_D_INVERTED;
+    1'b0: always @(posedge C) if (S == !IS_S_INVERTED) Q <= 1'b1; else if (CE) Q <= D ^ IS_D_INVERTED;
+    1'b1: always @(negedge C) if (S == !IS_S_INVERTED) Q <= 1'b1; else if (CE) Q <= D ^ IS_D_INVERTED;
   endcase endgenerate
 endmodule
 
