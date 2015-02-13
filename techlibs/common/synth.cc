04cb947d6a35b3773e694651971f28cbf83952e9
Clifford Wolf <clifford@clifford.at>
2015-02-13 14:34:51 +0100

    Added "check" command

--- techlibs/common/synth.cc
+++ techlibs/common/synth.cc
@@ -71,6 +71,8 @@ struct SynthPass : public Pass {
 		log("\n");
 		log("    coarse:\n");
 		log("        proc\n");
+		log("        opt_clean\n");
+		log("        check\n");
 		log("        opt\n");
 		log("        wreduce\n");
 		log("        alumacc\n");
@@ -150,6 +152,8 @@ struct SynthPass : public Pass {
 		if (check_label(active, run_from, run_to, "coarse"))
 		{
 			Pass::call(design, "proc");
+			Pass::call(design, "opt_clean");
+			Pass::call(design, "check");
 			Pass::call(design, "opt");
 			Pass::call(design, "wreduce");
 			Pass::call(design, "alumacc");
