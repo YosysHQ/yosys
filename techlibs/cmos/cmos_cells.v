e468d4cc6001b65b9c7e72d3f9c0e9b939ad31b9
Clifford Wolf <clifford@clifford.at>
2015-03-25 09:00:41 +0100

    Fixes in cmos_cells.v

--- techlibs/cmos/cmos_cells.v
+++ techlibs/cmos/cmos_cells.v
@@ -1,17 +1,26 @@
 
+module BUF(A, Y);
+input A;
+output Y;
+assign Y = A;
+endmodule
+
 module NOT(A, Y);
 input A;
-output Y = ~A;
+output Y;
+assign Y = ~A;
 endmodule
 
 module NAND(A, B, Y);
 input A, B;
-output Y = ~(A & B);
+output Y;
+assign Y = ~(A & B);
 endmodule
 
 module NOR(A, B, Y);
 input A, B;
-output Y = ~(A | B);
+output Y;
+assign Y = ~(A | B);
 endmodule
 
 module DFF(C, D, Q);
