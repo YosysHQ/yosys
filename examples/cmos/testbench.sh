dac807fb33a3619dea955ce4b16342e6e0008111
Clifford Wolf <clifford@clifford.at>
2016-03-11 11:30:01 +0100

    Cleanups and improvements in examples/cmos/

--- examples/cmos/testbench.sh
+++ examples/cmos/testbench.sh
@@ -5,6 +5,3 @@ set -ex
 ../../yosys counter.ys
 ngspice testbench.sp
 
-# requires ngspice with xspice support enabled:
-#ngspice testbench_digital.sp
-
--- examples/cmos/testbench_digital.sh
+++ examples/cmos/testbench_digital.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# iverlog simulation
-echo "Doing Verilog simulation with iverilog"
-iverilog -o dsn counter.v counter_tb.v 
-./dsn -lxt2
-gtkwave counter_tb.vcd &
-
-# yosys synthesis
 set -ex
 
+# iverlog simulation
+echo "Doing Verilog simulation with iverilog"
+iverilog -o counter_tb counter.v counter_tb.v 
+./counter_tb; gtkwave counter_tb.gtkw &
+
+# yosys synthesis
 ../../yosys counter_digital.ys
 
 # requires ngspice with xspice support enabled:
 ngspice testbench_digital.sp
+
