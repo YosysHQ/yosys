c2b909c051edf189d6e1f807bb367c3c543dc058
Andrew Zonenberg <azonenberg@drawersteak.com>
2016-04-04 21:46:07 -0700

    Added GP_POR

--- techlibs/greenpak4/cells_sim.v
+++ techlibs/greenpak4/cells_sim.v
@@ -153,3 +153,25 @@ module GP_BANDGAP(output reg OK, output reg VOUT);
 	//cannot simulate mixed signal IP
 	
 endmodule
+
+
+module GP_POR(output reg RST_DONE);
+	parameter POR_TIME = 500;
+	
+	initial begin
+		RST_DONE = 0;
+		
+		if(POR_TIME == 4)
+			#4000;
+		else if(POR_TIME == 500)
+			#500000;
+		else begin
+			$display("ERROR: bad POR_TIME for GP_POR cell");
+			$finish;
+		end
+		
+		RST_DONE = 1;
+		
+	end
+	
+endmodule
