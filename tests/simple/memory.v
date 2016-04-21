1761d08dd239bcf1765ebf807fc22514edac387f
Clifford Wolf <clifford@clifford.at>
2016-04-21 12:06:07 +0200

    Bugfix and improvements in memory_share

--- tests/simple/memory.v
+++ tests/simple/memory.v
@@ -243,3 +243,24 @@ module memtest10(input clk, input [5:0] din, output [5:0] dout);
 
 	assign dout = queue[3];
 endmodule
+
+// ----------------------------------------------------------
+
+module memtest11(clk, wen, waddr, raddr, wdata, rdata);
+	input clk, wen;
+	input [1:0] waddr, raddr;
+	input [7:0] wdata;
+	output [7:0] rdata;
+
+	reg [7:0] mem [3:0];
+
+	assign rdata = mem[raddr];
+
+	always @(posedge clk) begin
+		if (wen)
+			mem[waddr] <= wdata;
+		else
+			mem[waddr] <= mem[waddr];
+	end
+endmodule
+
