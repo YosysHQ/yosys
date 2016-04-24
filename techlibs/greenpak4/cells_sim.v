6e215f374dcd92e2c1bff8ad6114f3d3dc9b06f5
Andrew Zonenberg <azonenberg@drawersteak.com>
2016-04-23 22:53:49 -0700

    Renamed VOUT to OUT on GP_ACMP cell

--- techlibs/greenpak4/cells_sim.v
+++ techlibs/greenpak4/cells_sim.v
@@ -13,13 +13,15 @@ module GP_4LUT(input IN0, IN1, IN2, IN3, output OUT);
 	assign OUT = INIT[{IN3, IN2, IN1, IN0}];
 endmodule
 
-module GP_ACMP(input wire PWREN, input wire VIN, input wire VREF, output reg VOUT = 0);
+module GP_ACMP(input wire PWREN, input wire VIN, input wire VREF, output reg OUT);
 
 	parameter BANDWIDTH = "HIGH";
 	parameter VIN_BUF_EN = 0;
 	parameter VIN_ATTEN = 1;
 	parameter VIN_ISRC_EN = 0;
 	parameter HYSTERESIS = 0;
+	
+	initial OUT = 0;
 	
 	//cannot simulate mixed signal IP
 
