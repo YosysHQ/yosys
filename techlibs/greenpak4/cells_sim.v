349d7172023e9ee38b8e25e3bd04e2dfd0af1e62
Andrew Zonenberg <azonenberg@drawersteak.com>
2016-04-24 17:01:21 -0700

    Removed VIN_BUF_EN

--- techlibs/greenpak4/cells_sim.v
+++ techlibs/greenpak4/cells_sim.v
@@ -16,7 +16,6 @@ endmodule
 module GP_ACMP(input wire PWREN, input wire VIN, input wire VREF, output reg OUT);
 
 	parameter BANDWIDTH = "HIGH";
-	parameter VIN_BUF_EN = 0;
 	parameter VIN_ATTEN = 1;
 	parameter VIN_ISRC_EN = 0;
 	parameter HYSTERESIS = 0;
