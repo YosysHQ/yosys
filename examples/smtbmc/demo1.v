b3155af5f65333d272da339222e1e1962fb088b7
Clifford Wolf <clifford@clifford.at>
2016-07-13 09:49:05 +0200

    Added examples/smtbmc

---
+++ examples/smtbmc/demo1.v
@@ -0,0 +1,17 @@
+module demo1(input clk, input addtwo, output iseven);
+	reg [3:0] cnt = 0;
+	wire [3:0] next_cnt;
+
+	inc inc_inst (addtwo, iseven, cnt, next_cnt);
+
+	always @(posedge clk)
+		cnt = (iseven ? cnt == 10 : cnt == 11) ? 0 : next_cnt;
+	
+	assert property (cnt != 15);
+	// initial expect ((iseven && addtwo) || cnt == 9);
+endmodule
+
+module inc(input addtwo, output iseven, input [3:0] a, output [3:0] y);
+	assign iseven = !a[0];
+	assign y = a + (addtwo ? 2 : 1);
+endmodule
