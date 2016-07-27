9540be1d45cce45d0008a4160bc4aa70ff0dfe1d
Clifford Wolf <clifford@clifford.at>
2016-07-27 15:44:11 +0200

    Removed $predict support from SatGen

--- kernel/satgen.h
+++ kernel/satgen.h
@@ -69,7 +69,6 @@ struct SatGen
 	SigPool initial_state;
 	std::map<std::string, RTLIL::SigSpec> asserts_a, asserts_en;
 	std::map<std::string, RTLIL::SigSpec> assumes_a, assumes_en;
-	std::map<std::string, RTLIL::SigSpec> predict_a, predict_en;
 	std::map<std::string, std::map<RTLIL::SigBit, int>> imported_signals;
 	std::map<std::pair<std::string, int>, bool> initstates;
 	bool ignore_div_by_zero;
@@ -1371,14 +1370,6 @@ struct SatGen
 			std::string pf = prefix + (timestep == -1 ? "" : stringf("@%d:", timestep));
 			assumes_a[pf].append((*sigmap)(cell->getPort("\\A")));
 			assumes_en[pf].append((*sigmap)(cell->getPort("\\EN")));
-			return true;
-		}
-
-		if (cell->type == "$predict")
-		{
-			std::string pf = prefix + (timestep == -1 ? "" : stringf("@%d:", timestep));
-			predict_a[pf].append((*sigmap)(cell->getPort("\\A")));
-			predict_en[pf].append((*sigmap)(cell->getPort("\\EN")));
 			return true;
 		}
 
