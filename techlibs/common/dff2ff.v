e7a984a4dfc3248a23a752c8e92a7370c70af87a
Clifford Wolf <clifford@clifford.at>
2017-05-31 11:45:58 +0200

    Add dff2ff.v techmap file

---
+++ techlibs/common/dff2ff.v
@@ -0,0 +1,14 @@
+(* techmap_celltype = "$dff" *)
+module dff2ff (CLK, D, Q);
+	parameter WIDTH = 1;
+	parameter CLK_POLARITY = 1;
+
+	input CLK;
+	input [WIDTH-1:0] D;
+	output reg [WIDTH-1:0] Q;
+
+	wire [1023:0] _TECHMAP_DO_ = "proc;;";
+
+	always @($global_clock)
+		Q <= D;
+endmodule
