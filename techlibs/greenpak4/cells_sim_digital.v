60dd5dba7ba07b1992123681b37d6ffa6dd2dae4
Andrew Zonenberg <azonenberg@drawersteak.com>
2017-08-06 08:40:23 -0700

    Moved GP_POR out of digital cells b/c it has delays

--- techlibs/greenpak4/cells_sim_digital.v
+++ techlibs/greenpak4/cells_sim_digital.v
@@ -378,27 +378,6 @@ module GP_PGEN(input wire nRST, input wire CLK, output reg OUT);
 
 endmodule
 
-module GP_POR(output reg RST_DONE);
-	parameter POR_TIME = 500;
-
-	initial begin
-		RST_DONE = 0;
-
-		if(POR_TIME == 4)
-			#4000;
-		else if(POR_TIME == 500)
-			#500000;
-		else begin
-			$display("ERROR: bad POR_TIME for GP_POR cell");
-			$finish;
-		end
-
-		RST_DONE = 1;
-
-	end
-
-endmodule
-
 module GP_SHREG(input nRST, input CLK, input IN, output OUTA, output OUTB);
 
 	parameter OUTA_TAP = 1;
