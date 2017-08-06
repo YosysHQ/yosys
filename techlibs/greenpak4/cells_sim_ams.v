90b59915e6d6d92eb76bc9a7a27889c447c6c12b
Andrew Zonenberg <azonenberg@drawersteak.com>
2017-08-06 08:40:23 -0700

    Moved GP_POR out of digital cells b/c it has delays

--- techlibs/greenpak4/cells_sim_ams.v
+++ techlibs/greenpak4/cells_sim_ams.v
@@ -87,3 +87,24 @@ module GP_VREF(input VIN, output reg VOUT);
 	parameter VREF = 0;
 	//cannot simulate mixed signal IP
 endmodule
+
+module GP_POR(output reg RST_DONE);
+	parameter POR_TIME = 500;
+
+	initial begin
+		RST_DONE = 0;
+
+		if(POR_TIME == 4)
+			#4000;
+		else if(POR_TIME == 500)
+			#500000;
+		else begin
+			$display("ERROR: bad POR_TIME for GP_POR cell");
+			$finish;
+		end
+
+		RST_DONE = 1;
+
+	end
+
+endmodule
