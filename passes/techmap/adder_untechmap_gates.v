aac4436de34c8001f16fe3fbab89d184d9726a90
Robert Ou <rqou@robertou.com>
2017-08-11 00:16:07 -0700

    recover_adder: Replace XOR3 and MAJ gates with structural Verilog
    This way, they generate the uppercase cells rather than the lowercase
    cells, and it is possible to directly run abc afterwards. Before this
    change, it would be necessary to run simplemap first, which might
    destroy some other cells that have been recovered.

--- passes/techmap/adder_untechmap_gates.v
+++ passes/techmap/adder_untechmap_gates.v
@@ -1,7 +1,53 @@
 module __XOR3_ (input A, input B, input C, output Y);
-    assign Y = A ^ B ^ C;
+    wire intermed;
+
+    $_XOR_ xorgate1(
+        .A(A),
+        .B(B),
+        .Y(intermed),
+    );
+
+    $_XOR_ xorgate2(
+        .A(intermed),
+        .B(C),
+        .Y(Y),
+    );
 endmodule
 
 module __MAJ_ (input A, input B, input C, output Y);
-    assign Y = (A & B) | (A & C) | (B & C);
+    wire intermed1;
+    wire intermed2;
+    wire intermed3;
+
+    wire intermed4;
+
+    $_AND_ andgate1(
+        .A(A),
+        .B(B),
+        .Y(intermed1),
+    );
+
+    $_AND_ andgate2(
+        .A(A),
+        .B(C),
+        .Y(intermed2),
+    );
+
+    $_AND_ andgate3(
+        .A(B),
+        .B(C),
+        .Y(intermed3),
+    );
+
+    $_OR_ orgate1(
+        .A(intermed1),
+        .B(intermed2),
+        .Y(intermed4),
+    );
+
+    $_OR_ orgate2(
+        .A(intermed4),
+        .B(intermed3),
+        .Y(Y),
+    );
 endmodule
