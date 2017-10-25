9df0d2000b70b93425eeab2333cea136434a4405
Tim 'mithro' Ansell <mithro@mithis.com>
2017-10-25 13:08:15 -0700

    tests/realmath: Rework the test output.
     * Add a -v (for verbose) option, which outputs format identical to
       other tests.
     * Output dots for pass, 'F' for failure.
     * Output summary.

--- tests/realmath/run-test.sh
+++ tests/realmath/run-test.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-set -e
 
 OPTIND=1
 count=100
 seed=""    # default to no seed specified
-while getopts "c:S:" opt
+verbose=0
+while getopts "c:S:v" opt
 do
     case "$opt" in
 	c) count="$OPTARG" ;;
 	S) seed="-S $OPTARG" ;;
+	v) verbose="1" ;;
     esac
 done
 shift "$((OPTIND-1))"
@@ -19,18 +20,51 @@ echo "generating tests.."
 python3 generate.py -c $count $seed
 
 cd temp
-echo "running tests.."
+echo -n "running tests.. "
+if test $verbose -gt 0; then
+	echo
+fi
+failed=0
+passed=0
 for ((i = 0; i < $count; i++)); do
-	echo -n "[$i]"
+	if test $verbose -gt 0; then
+		echo -n "Test: realmath[$i] -> "
+	fi
 	idx=$( printf "%05d" $i )
 	../../../yosys -qq uut_${idx}.ys
 	iverilog -o uut_${idx}_tb uut_${idx}_tb.v uut_${idx}.v uut_${idx}_syn.v
-	./uut_${idx}_tb | tee uut_${idx}.err
-	if test -s uut_${idx}.err; then
+	if test ! -s ./uut_${idx}_tb; then
 		echo "Note: Make sure that 'iverilog' is an up-to-date git checkout of Icarus Verilog."
 		exit 1
 	fi
-	rm -f uut_${idx}.err
+	./uut_${idx}_tb > uut_${idx}.err
+	test_exit_code=$?
+	if test $verbose -gt 0; then
+		if test $test_exit_code -ne 0; then
+			echo "fail"
+			echo "---------------------"
+			cat uut_${idx}.err
+			echo "---------------------"
+		else
+			echo "ok"
+		fi
+	else
+		if test $test_exit_code -ne 0; then
+			echo -n "F"
+		else
+			echo -n "."
+		fi
+	fi
+	if test $test_exit_code -ne 0; then
+		failed=$(($failed + 1))
+	else
+		passed=$(($passed + 1))
+	fi
+	rm -f uut_${idx}.err uut_${idx}_tb
 done
 echo
+echo "$count tests run, $passed passed, $failed failed."
 
+if test $failed -ne 0; then
+	exit 1
+fi
