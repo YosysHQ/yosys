d8003e87d1c5962e660ce84150239444b7fb037d
David Shah <dave@ds0.me>
2019-01-21 12:34:34 +0000

    ecp5: More blackboxes
    Signed-off-by: David Shah <dave@ds0.me>

--- techlibs/ecp5/cells_bb.v
+++ techlibs/ecp5/cells_bb.v
@@ -156,6 +156,23 @@ module OSCG(
 parameter DIV = 128;
 endmodule
 
+(* blackbox *) (* keep *)
+module USRMCLK(
+	input USRMCLKI, USRMCLKTS,
+	output USRMCLKO
+);
+endmodule
+
+(* blackbox *) (* keep *)
+module JTAGG(
+	input TCK, TMS, TDI, JTDO2, JTDO1,
+	output TDO, JTDI, JTCK, JRTI2, JRTI1,
+	output JSHIFT, JUPDATE, JRSTN, JCE2, JCE1
+);
+parameter ER1 = "ENABLED";
+parameter ER2 = "ENABLED";
+endmodule
+
 (* blackbox *)
 module IDDRX1F(
 	input D, SCLK, RST,
