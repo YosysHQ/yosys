cbb85e40e87fbfb1602bb934ed76a97efb9e55c6
Eddie Hung <eddie@fpgeh.com>
2019-04-16 14:53:28 -0700

    Add MUXCY and XORCY to cells_box.v

--- techlibs/xilinx/cells_box.v
+++ techlibs/xilinx/cells_box.v
@@ -8,3 +8,12 @@ module MUXF8(output O, input I0, I1, S);
   assign O = S ? I1 : I0;
 endmodule
 
+(* abc_box_id = 3 *)
+module MUXCY(output O, input CI, DI, S);
+  assign O = S ? CI : DI;
+endmodule
+
+(* abc_box_id = 4 *)
+module XORCY(output O, input CI, LI);
+  assign O = CI ^ LI;
+endmodule
