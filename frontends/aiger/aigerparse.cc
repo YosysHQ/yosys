705775342774cff735ba13d2c3216680ae98b7a0
Eddie Hung <eddie@fpgeh.com>
2019-05-21 18:20:31 -0700

    Rename label

--- frontends/aiger/aigerparse.cc
+++ frontends/aiger/aigerparse.cc
@@ -55,16 +55,15 @@ void AigerReader::parse_aiger()
 
     // Optional values
     B = C = J = F = 0;
-    if (f.peek() != ' ') goto parse_body;
+    if (f.peek() != ' ') goto end_of_header;
     if (!(f >> B)) log_error("Invalid AIGER header\n");
-    if (f.peek() != ' ') goto parse_body;
+    if (f.peek() != ' ') goto end_of_header;
     if (!(f >> C)) log_error("Invalid AIGER header\n");
-    if (f.peek() != ' ') goto parse_body;
+    if (f.peek() != ' ') goto end_of_header;
     if (!(f >> J)) log_error("Invalid AIGER header\n");
-    if (f.peek() != ' ') goto parse_body;
+    if (f.peek() != ' ') goto end_of_header;
     if (!(f >> F)) log_error("Invalid AIGER header\n");
-
-parse_body:
+end_of_header:
 
     std::string line;
     std::getline(f, line); // Ignore up to start of next line, as standard
