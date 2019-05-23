c2caf85f7cbcbea4240b56a134e4c3e74189c62d
Stefan Biereigel <stefan.biereigel@cern.ch>
2019-05-23 13:42:42 +0200

    add simple test case for wand/wor

---
+++ tests/various/wandwor.v
@@ -0,0 +1,35 @@
+module a(Q);
+    output wire Q;
+
+    assign Q = 0;
+endmodule
+
+module b(D);
+    input wire D;
+endmodule
+
+module c;
+    wor D;
+    assign D = 1;
+    assign D = 0;
+    assign D = 1;
+    assign D = 0;
+
+
+    wand E;
+    wire E_wire = E;
+    
+    genvar i;
+    for (i = 0; i < 3; i = i + 1)
+    begin :genloop
+        a a_inst (
+            .Q(E)
+        );
+        
+        b b_inst (
+            .D(E_wire)
+        );
+    end
+
+endmodule
+
