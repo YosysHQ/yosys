f68b658b4b88b9a71377d19d7d693f07eccf433e
Stefan Biereigel <stefan@biereigel.de>
2019-05-27 18:45:54 +0200

    reformat wand/wor test

--- tests/various/wandwor.v
+++ tests/various/wandwor.v
@@ -1,34 +1,33 @@
 module a(Q);
-    output wire Q;
-
-    assign Q = 0;
+	output wire Q = 0;
 endmodule
 
 module b(D);
-    input wire D;
+	input wire D;
 endmodule
 
 module c;
-    wor D;
-    assign D = 1;
-    assign D = 0;
-    assign D = 1;
-    assign D = 0;
+	// net definitions
+	wor D;
+	wand E;
 
+	// assignments to wired logic nets
+	assign D = 1;
+	assign D = 0;
+	assign D = 1;
+	assign D = 0;
 
-    wand E;
-    
-    genvar i;
-    for (i = 0; i < 3; i = i + 1)
-    begin :genloop
-        a a_inst (
-            .Q(E)
-        );
-        
-        b b_inst (
-            .D(E)
-        );
-    end
+	// assignments of wired logic nets to wires
+	wire F = E;
 
+	genvar i;
+	for (i = 0; i < 3; i = i + 1)
+	begin : genloop
+		// connection of module outputs
+		a a_inst (.Q(E));
+
+		// connection of module inputs
+		b b_inst (.D(E));
+	end
 endmodule
 
