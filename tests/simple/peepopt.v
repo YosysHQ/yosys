cb285e4b87112779efe84a7b6c5ae4a76a69ae01
Clifford Wolf <clifford@clifford.at>
2019-05-28 15:33:47 +0200

    Do not use shiftmul peepopt pattern when mul result is truncated, fixes #1047
    Signed-off-by: Clifford Wolf <clifford@clifford.at>

--- tests/simple/peepopt.v
+++ tests/simple/peepopt.v
@@ -2,6 +2,10 @@ module peepopt_shiftmul_0 #(parameter N=3, parameter W=3) (input [N*W-1:0] i, in
 assign o = i[s*W+:W];
 endmodule
 
+module peepopt_shiftmul_1 (output y, input [2:0] w);
+assign y = 1'b1 >> (w * (3'b110));
+endmodule
+
 module peepopt_muldiv_0(input [1:0] i, output [1:0] o);
 wire [3:0] t;
 assign t = i * 3;
