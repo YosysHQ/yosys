65924fd12f48b4ec5a4d51efeea977992d033ecf
Eddie Hung <eddie@fpgeh.com>
2019-06-07 11:28:05 -0700

    Test *.aag too, by using *.aig as reference

--- tests/aiger/run-test.sh
+++ tests/aiger/run-test.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
 set -e
+
+for aag in *.aag; do
+    # Since ABC cannot read *.aag, read the *.aig instead
+    # (which would have been created by the reference aig2aig utility)
+    ../../yosys-abc -c "read -c ${aag%.*}.aig; write ${aag%.*}_ref.v"
+    ../../yosys -p "
+read_verilog ${aag%.*}_ref.v
+prep
+design -stash gold
+read_aiger -clk_name clock $aag
+prep
+design -stash gate
+design -import gold -as gold
+design -import gate -as gate
+miter -equiv -flatten -make_assert -make_outputs gold gate miter
+sat -verify -prove-asserts -show-ports -seq 16 miter
+"
+done
+
 for aig in *.aig; do
     ../../yosys-abc -c "read -c $aig; write ${aig%.*}_ref.v"
     ../../yosys -p "
