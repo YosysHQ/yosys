1da12c5071a738504d22e68d66cab7c5c5afb07e
Eddie Hung <eddie@fpgeh.com>
2019-06-07 12:12:11 -0700

    Add @cliffordwolf freduce testcase

--- tests/various/muxpack.v
+++ tests/various/muxpack.v
@@ -166,3 +166,16 @@ module cliffordwolf_nonexclusive_select (
                 if (z) o = d;
         end
 endmodule
+
+module cliffordwolf_freduce (
+        input wire [1:0] s,
+        input wire a, b, c, d,
+        output reg [3:0] o
+);
+        always @* begin
+                o = {4{a}};
+                if (s == 0) o = {3{b}};
+                if (s == 1) o = {2{c}};
+                if (s == 2) o = d;
+        end
+endmodule
