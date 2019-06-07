b959bf79c004fdf81ccc397d5aa774b67a09d6da
Eddie Hung <eddie@fpgeh.com>
2019-06-07 15:35:15 -0700

    Add nonexcl case test, comment out two others

--- tests/various/muxpack.v
+++ tests/various/muxpack.v
@@ -179,3 +179,21 @@ module cliffordwolf_freduce (
                 if (s == 2) o = d;
         end
 endmodule
+
+module case_nonexclusive_select (
+        input wire [1:0] x, y,
+        input wire a, b, c, d, e,
+        output reg o
+);
+        always @* begin
+            case (x)
+                0, 2: o = b;
+                1: o = c;
+                default: begin
+                    o = a;
+                    if (y == 0) o = d;
+                    if (y == 1) o = e;
+                end
+        endcase
+        end
+endmodule
