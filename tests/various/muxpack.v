e263bc249b905195120fbc074c6f80d03fb21cf8
Eddie Hung <eddie@fpgeh.com>
2019-06-07 11:54:29 -0700

    Add nonexclusive test from @cliffordwolf

--- tests/various/muxpack.v
+++ tests/various/muxpack.v
@@ -153,3 +153,16 @@ always @*
     else
      o <= i[4*W+:W];
 endmodule
+
+module cliffordwolf_nonexclusive_select (
+        input wire x, y, z,
+        input wire a, b, c, d,
+        output reg o
+);
+        always @* begin
+                o = a;
+                if (x) o = b;
+                if (y) o = c;
+                if (z) o = d;
+        end
+endmodule
