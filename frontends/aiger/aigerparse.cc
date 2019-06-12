b21d29598a59f0f137a42f00a000b7937dabb402
Eddie Hung <eddie@fpgeh.com>
2019-06-12 09:40:51 -0700

    Consistency

--- frontends/aiger/aigerparse.cc
+++ frontends/aiger/aigerparse.cc
@@ -2,7 +2,7 @@
  *  yosys -- Yosys Open SYnthesis Suite
  *
  *  Copyright (C) 2012  Clifford Wolf <clifford@clifford.at>
- *  Copyright (C) 2019  Eddie Hung <eddie@fpgeh.com>
+ *                2019  Eddie Hung <eddie@fpgeh.com>
  *
  *  Permission to use, copy, modify, and/or distribute this software for any
  *  purpose with or without fee is hereby granted, provided that the above
