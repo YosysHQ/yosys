f433a523742350b4bb9291ad65299015e2c81f05
Eddie Hung <eddie@fpgeh.com>
2019-06-21 11:15:23 -0700

    Add FIXME about need for -mux4

--- techlibs/xilinx/synth_xilinx.cc
+++ techlibs/xilinx/synth_xilinx.cc
@@ -275,6 +275,8 @@ struct SynthXilinxPass : public ScriptPass
 			run("dff2dffe");
 			if (!nomux || help_mode) {
 				run("simplemap t:$mux", "                     (skip if -nomux)");
+				              // FIXME: Must specify mux4, even if we don't need it,
+					      //        otherwise it will use mux8 as mux4
 				run("muxcover -mux4=150 -mux8=200 -mux16=250 -dmux=0", "(skip if -nomux)");
 			}
 			run("opt -full");
