18acb72c05d270e2224ce55919cfb0524efb5757
Eddie Hung <eddie@fpgeh.com>
2019-06-27 11:02:52 -0700

    Add #1135 testcase

--- tests/various/pmux2shiftx.v
+++ tests/various/pmux2shiftx.v
@@ -32,3 +32,13 @@ module pmux2shiftx_test (
 		endcase
 	end
 endmodule
+
+module issue01135(input [7:0] i, output o);
+always @*
+case (i[6:3])
+    4: o <= i[0];
+    3: o <= i[2];
+    7: o <= i[3];
+    default: o <= 1'b0;
+endcase
+endmodule
