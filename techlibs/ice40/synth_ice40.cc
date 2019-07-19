3dc3c749d5c7c10e1aa504f48794ef0a87513a82
Eddie Hung <eddie@fpgeh.com>
2019-07-19 11:41:00 -0700

    Add DSP_MINWIDTH=11 for ice40 since ice40_dsp uses this threshold

--- techlibs/ice40/synth_ice40.cc
+++ techlibs/ice40/synth_ice40.cc
@@ -266,7 +266,7 @@ struct SynthIce40Pass : public ScriptPass
 			run("opt_expr");
 			run("opt_clean");
 			if (help_mode || dsp) {
-				run("techmap -map +/mul2dsp.v -D DSP_A_MAXWIDTH=16 -D DSP_B_MAXWIDTH=16 -D DSP_NAME=$__MUL16X16");
+				run("techmap -map +/mul2dsp.v -D DSP_A_MAXWIDTH=16 -D DSP_B_MAXWIDTH=16 -D DSP_MINWIDTH=11 -D DSP_NAME=$__MUL16X16", "(if -dsp)");
 				run("ice40_dsp", "(if -dsp)");
 			}
 			run("alumacc");
