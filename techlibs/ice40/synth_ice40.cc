5e70b8a22bf38e622943b0546255befd539fa884
Eddie Hung <eddie@fpgeh.com>
2019-07-22 13:48:33 -0700

    opt and wreduce necessary for -dsp

--- techlibs/ice40/synth_ice40.cc
+++ techlibs/ice40/synth_ice40.cc
@@ -267,8 +267,10 @@ struct SynthIce40Pass : public ScriptPass
 			run("opt_clean");
 			if (help_mode || dsp) {
 				run("techmap -map +/mul2dsp.v -D DSP_A_MAXWIDTH=16 -D DSP_B_MAXWIDTH=16 -D DSP_MINWIDTH=11 -D DSP_NAME=$__MUL16X16", "(if -dsp)");
-				run("ice40_dsp", "(if -dsp)");
-				run("chtype -set $mul t:$__soft_mul");
+				run("opt_expr", "                     (if -dsp)");
+				run("wreduce", "                      (if -dsp)");
+				run("ice40_dsp", "                    (if -dsp)");
+				run("chtype -set $mul t:$__soft_mul","(if -dsp)");
 			}
 			run("alumacc");
 			run("opt");
