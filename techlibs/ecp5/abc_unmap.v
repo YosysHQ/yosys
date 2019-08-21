6b1b03d9f771addbd54358299faad181b589c9f8
Eddie Hung <eddie@fpgeh.com>
2019-08-20 19:20:17 -0700

    ecp5: remove DPR16X4 from abc_unmap.v

--- techlibs/ecp5/abc_unmap.v
+++ techlibs/ecp5/abc_unmap.v
@@ -3,23 +3,3 @@
 module \$__ABC_DPR16X4_COMB (input [3:0] A, S, output [3:0] Y);
     assign Y = A;
 endmodule
-
-module \$__ABC_DPR16X4_SEQ (
-	input  [3:0] DI,
-	input  [3:0] WAD,
-	input        WRE,
-	input        WCK,
-	input  [3:0] RAD,
-	output [3:0] DO
-);
-	parameter WCKMUX = "WCK";
-	parameter WREMUX = "WRE";
-	parameter [63:0] INITVAL = 64'h0000000000000000;
-
-    TRELLIS_DPR16X4 #(
-      .WCKMUX(WCKMUX), .WREMUX(WREMUX), .INITVAL(INITVAL)
-    ) _TECHMAP_REPLACE_ (
-      .DI(DI), .WAD(WAD), .WRE(WRE), .WCK(WCK),
-      .RAD(RAD), .DO(DO)
-    );
-endmodule
