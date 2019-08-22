5061d239ae2d6881b898662f838a0f4cfc24ca88
Eddie Hung <eddie@fpgeh.com>
2019-08-22 14:03:02 -0700

    Fail if iverilog fails

--- tests/ice40/run-test.sh
+++ tests/ice40/run-test.sh
@@ -18,8 +18,8 @@ for t in *_tb.v; do
 	echo "all:: run-$t"
 	echo "run-$t:"
 	echo "	@echo 'Running $t..'"
-	echo "	@iverilog -o ${t%_tb.v}_testbench  $t ${t%_tb.v}_synth.v common.v $COMMON_PREFIX/simcells.v $TECHLIBS_PREFIX/ice40/cells_sim.v"
-	echo "	@if ! vvp -N ${t%_tb.v}_testbench > ${t%_tb.v}_testbench.log 2>&1; then grep 'ERROR' ${t%_tb.v}_testbench.log; exit 0; elif grep 'ERROR' ${t%_tb.v}_testbench.log || ! grep 'OKAY' ${t%_tb.v}_testbench.log; then echo "FAIL"; exit 0; fi"
+	echo "	@iverilog -o ${t%_tb.v}_testbench  $t ${t%_tb.v}_synth.v common.v $TECHLIBS_PREFIX/ice40/cells_sim.v"
+	echo "	@vvp -N ${t%_tb.v}_testbench"
 done
 for s in *.sh; do
 	if [ "$s" != "run-test.sh" ]; then
