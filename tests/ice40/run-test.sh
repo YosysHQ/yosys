8e3abda19308bd4ac7e03d38f899831e853f4794
SergeyDegtyar <sndegtyar@gmail.com>
2019-08-30 09:11:03 +0300

    Remove simulation from run-test.sh (unnecessary paths)

--- tests/ice40/run-test.sh
+++ tests/ice40/run-test.sh
@@ -1,11 +1,5 @@
+#!/usr/bin/env bash
 set -e
-if [ -f "../../techlibs/common/simcells.v" ]; then
-	COMMON_PREFIX=../../techlibs/common
-	TECHLIBS_PREFIX=../../techlibs
-else
-	COMMON_PREFIX=/usr/local/share/yosys
-	TECHLIBS_PREFIX=/usr/local/share/yosys
-fi
 {
 echo "all::"
 for x in *.ys; do
@@ -14,14 +8,13 @@ for x in *.ys; do
 	echo "	@echo 'Running $x..'"
 	echo "	@../../yosys -ql ${x%.ys}.log $x -w 'Yosys has only limited support for tri-state logic at the moment.'"
 done
-
-#for s in *.sh; do
-#	if [ "$s" != "run-test.sh" ]; then
-#		echo "all:: run-$s"
-#		echo "run-$s:"
-#		echo "	@echo 'Running $s..'"
-#		echo "	@bash $s"
-#	fi
-#done
+for s in *.sh; do
+	if [ "$s" != "run-test.sh" ]; then
+		echo "all:: run-$s"
+		echo "run-$s:"
+		echo "	@echo 'Running $s..'"
+		echo "	@bash $s"
+	fi
+done
 } > run-test.mk
 exec ${MAKE:-make} -f run-test.mk
