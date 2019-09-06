5a2fc6fcb5141573cbfcebdec4354fc11056a8f4
Eddie Hung <eddie@fpgeh.com>
2019-09-05 18:06:59 -0700

    Refactor ice40_dsp

--- passes/pmgen/ice40_dsp.cc
+++ passes/pmgen/ice40_dsp.cc
@@ -74,9 +74,11 @@ void create_ice40_dsp(ice40_dsp_pm &pm)
 
 	// SB_MAC16 Input Interface
 	SigSpec A = st.sigA;
+	A.extend_u0(16, st.mul->getParam("\\A_SIGNED").as_bool());
 	log_assert(GetSize(A) == 16);
 
 	SigSpec B = st.sigB;
+	B.extend_u0(16, st.mul->getParam("\\B_SIGNED").as_bool());
 	log_assert(GetSize(B) == 16);
 
 	SigSpec CD = st.sigCD;
