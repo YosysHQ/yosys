65fa8adf6c834cc3c73300a19d4fe96c31b8d361
Eddie Hung <eddie@fpgeh.com>
2019-09-19 11:02:14 -0700

    Format macc.v

--- tests/ice40/macc.v
+++ tests/ice40/macc.v
@@ -13,13 +13,13 @@ reg [(A_WIDTH + B_WIDTH - 1):0] reg_tmp_c;
 assign c = reg_tmp_c;
 always @(posedge clk)
 begin
-if(set)
-begin
-reg_tmp_c <= 0;
-end
-else
-begin
-reg_tmp_c <= a * b + c;
-end
+    if(set)
+    begin
+        reg_tmp_c <= 0;
+    end
+    else
+    begin
+        reg_tmp_c <= a * b + c;
+    end
 end
 endmodule
