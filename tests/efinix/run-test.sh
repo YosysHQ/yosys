eb750670e3835a1bad36cb604e04bf4836cc7f91
Sergey <37293587+SergeyDegtyar@users.noreply.github.com>
2019-10-01 11:14:12 +0300

    run-test.sh Move $x at end of line.

--- tests/efinix/run-test.sh
+++ tests/efinix/run-test.sh
@@ -6,7 +6,7 @@ for x in *.ys; do
 	echo "all:: run-$x"
 	echo "run-$x:"
 	echo "	@echo 'Running $x..'"
-	echo "	@../../yosys -ql ${x%.ys}.log $x"
+	echo "	@../../yosys -ql ${x%.ys}.log -w 'Yosys has only limited support for tri-state logic at the moment.' $x"
 done
 for s in *.sh; do
 	if [ "$s" != "run-test.sh" ]; then
