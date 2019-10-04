3d3479b0af0d2d59708a0828cfa785257c52d6fd
Miodrag Milanovic <mmicko@gmail.com>
2019-10-04 10:57:47 +0200

    Cleanup top modules and not used defines

--- tests/anlogic/tribuf.v
+++ tests/anlogic/tribuf.v
@@ -6,18 +6,3 @@ module tristate (en, i, o);
 	assign o = en ? i : 1'bz;
 
 endmodule
-
-
-module top (
-input en,
-input a,
-output b
-);
-
-tristate u_tri (
-        .en (en ),
-        .i (a ),
-        .o (b )
-    );
-
-endmodule
