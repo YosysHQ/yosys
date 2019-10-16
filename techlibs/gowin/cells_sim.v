72323e11a4ee222c0ce928669d33333c46fb25aa
Pepijn de Vos <pepijndevos@gmail.com>
2019-10-16 11:24:56 +0200

    remove duplicate DFFR

--- techlibs/gowin/cells_sim.v
+++ techlibs/gowin/cells_sim.v
@@ -38,16 +38,6 @@ module DFFN (output reg Q, input CLK, D);
 		Q <= D;
 endmodule
 
-module DFFR (output reg Q, input D, CLK, RESET);
-	parameter [0:0] INIT = 1'b0;
-	initial Q = INIT;
-	always @(posedge CLK) begin
-        if (RESET)
-                Q <= 1'b0;
-        else
-                Q <= D;
-	end
-endmodule // DFFR (positive clock edge; synchronous reset)
 
 module DFFE (output reg Q, input D, CLK, CE);
   parameter [0:0] INIT = 1'b0;
