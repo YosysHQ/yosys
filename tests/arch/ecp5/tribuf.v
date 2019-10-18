5603595e5c0efd2afc9ba810e6e5992e5d81d44c
Miodrag Milanovic <mmicko@gmail.com>
2019-10-18 12:19:59 +0200

    Share common tests

--- tests/arch/ecp5/tribuf.v
+++
@@ -1,8 +0,0 @@
-module tristate (en, i, o);
-    input en;
-    input i;
-    output o;
-
-	assign o = en ? i : 1'bz;
-
-endmodule
--- tests/arch/xilinx/tribuf.v
+++
@@ -1,8 +0,0 @@
-module tristate (en, i, o);
-    input en;
-    input i;
-    output reg o;
-    
-    always @(en or i)
-		o <= (en)? i : 1'bZ;
-endmodule
