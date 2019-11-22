6841e3b1c2b2bc3124810f3a8f96ed00a96e954c
Eddie Hung <eddie@fpgeh.com>
2019-11-21 16:33:20 -0800

    Another sloppy mistake!

--- tests/various/abc9.v
+++ tests/various/abc9.v
@@ -13,6 +13,6 @@ endmodule
 module abc9_test032(input clk, d, r, output reg q);
 initial q = 1'b0;
 always @(negedge clk or negedge r)
-    if (r) q <= 1'b0;
+    if (!r) q <= 1'b0;
     else q <= d;
 endmodule
