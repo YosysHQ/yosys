97b22413e533a7fca7a3542dd802321097bdc55c
Martin Pietryka <martin@pietryka.at>
2019-11-23 06:18:23 +0100

    coolrunner2: remove spurious log_pop() call, fixes #1463
    This was causing a segmentation fault because there is no accompanying
    log_push() call so header_count.size() became -1.
    
    Signed-off-by: Martin Pietryka <martin@pietryka.at>

--- techlibs/coolrunner2/synth_coolrunner2.cc
+++ techlibs/coolrunner2/synth_coolrunner2.cc
@@ -194,8 +194,6 @@ struct SynthCoolrunner2Pass : public ScriptPass
 			if (!json_file.empty() || help_mode)
 				run(stringf("write_json %s", help_mode ? "<file-name>" : json_file.c_str()));
 		}
-
-		log_pop();
 	}
 } SynthCoolrunner2Pass;
 
