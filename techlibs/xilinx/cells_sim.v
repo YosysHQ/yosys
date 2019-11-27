e056ba5f91d863cad4279e8d7372d76e6099b55f
Alessandro Comodi <acomodi@antmicro.com>
2019-11-27 11:36:10 +0100

    fix carry4_cout definition
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- techlibs/xilinx/cells_sim.v
+++ techlibs/xilinx/cells_sim.v
@@ -463,7 +463,15 @@ assign COUT = CIN;
 
 endmodule
 
-module CARRY4_COUT(output [3:0] O, output COUT, input CI, input CYINIT, input [3:0] DI, S);
+(* abc_box_id = 8, lib_whitebox *)
+module CARRY4_COUT(
+    output [3:0] O,
+    (* abc_carry *)
+    output COUT,
+    (* abc_carry *)
+    input CI,
+    input CYINIT,
+    input [3:0] DI, S);
 `ifdef _ABC
   wire CI0 = CYINIT | CI;
 `else
