6b384861e4b1e02b24bf11d266cf11f461115cd8
Eddie Hung <eddie@fpgeh.com>
2019-12-16 13:31:05 -0800

    Oops

--- passes/memory/memory_bram.cc
+++ passes/memory/memory_bram.cc
@@ -333,12 +333,9 @@ struct rules_t
 				for (int idx = 1; idx <= GetSize(tokens)-1; idx++) {
 					size_t c1 = tokens[1][0] == '!' ? 1 : 0;
 					size_t c2 = tokens[1].find("=");
-					if (c2 != std::string::npos)
-						c2--;
-
 					bool exists = (c1 == 0);
 					IdString key = RTLIL::escape_id(tokens[1].substr(c1, c2));
-					Const val = c2 != std::string::npos ? tokens[1].substr(c2) : RTLIL::Const(1);
+					Const val = c2 != std::string::npos ? tokens[1].substr(c2+1) : RTLIL::Const(1);
 
 					data.attributes.back().emplace_back(exists, key, val);
 				}
