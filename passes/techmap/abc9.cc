556ed0e18ac00e4ec19e127f33ccf3550be78186
Eddie Hung <eddie@fpgeh.com>
2020-01-11 17:05:30 -0800

    MIssed this merge conflict

--- passes/techmap/abc9.cc
+++ passes/techmap/abc9.cc
@@ -304,10 +304,6 @@ void abc9_module(RTLIL::Design *design, RTLIL::Module *module, std::string scrip
 	for (size_t pos = abc9_script.find("{R}"); pos != std::string::npos; pos = abc9_script.find("{R}", pos))
 		abc9_script = abc9_script.substr(0, pos) + R + abc9_script.substr(pos+3);
 
-	if (nomfs)
-		for (size_t pos = abc9_script.find("&mfs"); pos != std::string::npos; pos = abc9_script.find("&mfs", pos))
-			abc9_script = abc9_script.erase(pos, strlen("&mfs"));
-
 	abc9_script += stringf("; &ps -l; &write -n %s/output.aig;", tempdir_name.c_str());
 	if (design->scratchpad_get_bool("abc9.verify")) {
 		if (dff_mode)
