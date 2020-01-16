caf35896da98225518f06ebcd57b010c958de652
Jeff Wang <jjj11x@gmail.com>
2020-01-16 12:03:42 -0500

    enum in package test

--- tests/svtypes/typedef_package.sv
+++ tests/svtypes/typedef_package.sv
@@ -1,11 +1,14 @@
 package pkg;
 	typedef logic [7:0] uint8_t;
+	typedef enum logic [7:0] {bb=8'hBB} enum8_t;
 endpackage
 
 module top;
 
 	(* keep *) (pkg::uint8_t) a = 8'hAA;
+	(* keep *) (pkg::enum8_t) b_enum = pkg::bb;
 
 	always @* assert(a == 8'hAA);
+	always @* assert(b_enum == 8'hBB);
 
 endmodule
