db68e4c2a7a39eda46863fba8b8c8313a831f606
Eddie Hung <eddie@fpgeh.com>
2020-01-17 16:08:04 -0800

    ice40_dsp: fix typo

--- passes/pmgen/ice40_dsp.cc
+++ passes/pmgen/ice40_dsp.cc
@@ -73,11 +73,11 @@ void create_ice40_dsp(ice40_dsp_pm &pm)
 
 	// SB_MAC16 Input Interface
 	SigSpec A = st.sigA;
-	A.extend_u0(16, st.mul->connections_.at(ID(A_SIGNED), State::S0).as_bool());
+	A.extend_u0(16, st.mul->parameters.at(ID(A_SIGNED), State::S0).as_bool());
 	log_assert(GetSize(A) == 16);
 
 	SigSpec B = st.sigB;
-	B.extend_u0(16, st.mul->connections_.at(ID(B_SIGNED), State::S0).as_bool());
+	B.extend_u0(16, st.mul->parameters.at(ID(B_SIGNED), State::S0).as_bool());
 	log_assert(GetSize(B) == 16);
 
 	SigSpec CD = st.sigCD;
