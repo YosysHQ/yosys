8217f579b7450f282f1baa8d6fe1fbefc002a534
Rodrigo Alejandro Melo <rodrigomelo9@gmail.com>
2020-02-02 12:33:34 -0300

    Removed 'synth' into tests/memfile/run-test.sh
    Signed-off-by: Rodrigo Alejandro Melo <rodrigomelo9@gmail.com>

--- tests/memfile/run-test.sh
+++ tests/memfile/run-test.sh
@@ -6,24 +6,24 @@ cp content1.dat temp/content2.dat
 cd ..
 
 echo "Running from the parent directory with content1.dat"
-../yosys -qp "read_verilog -defer memfile/memory.v; chparam -set MEMFILE \"content1.dat\" memory; synth -top memory"
+../yosys -qp "read_verilog -defer memfile/memory.v; chparam -set MEMFILE \"content1.dat\" memory"
 echo "Running from the parent directory with temp/content2.dat"
-../yosys -qp "read_verilog -defer memfile/memory.v; chparam -set MEMFILE \"temp/content2.dat\" memory; synth -top memory"
+../yosys -qp "read_verilog -defer memfile/memory.v; chparam -set MEMFILE \"temp/content2.dat\" memory"
 echo "Running from the parent directory with memfile/temp/content2.dat"
-../yosys -qp "read_verilog -defer memfile/memory.v; chparam -set MEMFILE \"memfile/temp/content2.dat\" memory; synth -top memory"
+../yosys -qp "read_verilog -defer memfile/memory.v; chparam -set MEMFILE \"memfile/temp/content2.dat\" memory"
 
 cd memfile
 
 echo "Running from the same directory with content1.dat"
-../../yosys -qp "read_verilog -defer memory.v; chparam -set MEMFILE \"content1.dat\" memory; synth -top memory"
+../../yosys -qp "read_verilog -defer memory.v; chparam -set MEMFILE \"content1.dat\" memory"
 echo "Running from the same directory with temp/content2.dat"
-../../yosys -qp "read_verilog -defer memory.v; chparam -set MEMFILE \"temp/content2.dat\" memory; synth -top memory"
+../../yosys -qp "read_verilog -defer memory.v; chparam -set MEMFILE \"temp/content2.dat\" memory"
 
 cd temp
 
 echo "Running from a child directory with content1.dat"
-../../../yosys -qp "read_verilog -defer ../memory.v; chparam -set MEMFILE \"content1.dat\" memory; synth -top memory"
+../../../yosys -qp "read_verilog -defer ../memory.v; chparam -set MEMFILE \"content1.dat\" memory"
 echo "Running from a child directory with temp/content2.dat"
-../../../yosys -qp "read_verilog -defer ../memory.v; chparam -set MEMFILE \"temp/content2.dat\" memory; synth -top memory"
+../../../yosys -qp "read_verilog -defer ../memory.v; chparam -set MEMFILE \"temp/content2.dat\" memory"
 echo "Running from a child directory with content2.dat"
-../../../yosys -qp "read_verilog -defer ../memory.v; chparam -set MEMFILE \"temp/content2.dat\" memory; synth -top memory"
+../../../yosys -qp "read_verilog -defer ../memory.v; chparam -set MEMFILE \"temp/content2.dat\" memory"
