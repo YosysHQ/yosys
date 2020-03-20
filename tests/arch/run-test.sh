0e97a968c20c42ad2b9a7484b3c7941a2d38c8bb
Eddie Hung <eddie@fpgeh.com>
2020-03-20 14:53:48 -0700

    tests: avoid using bash4's assoc arrays as macOS no likey

--- tests/arch/run-test.sh
+++ tests/arch/run-test.sh
@@ -2,14 +2,16 @@
 
 set -e
 
-declare -A defines=( ["ice40"]="ICE40_HX ICE40_LP ICE40_U" )
-
 echo "Running syntax check on arch sim models"
 for arch in ../../techlibs/*; do
 	find $arch -name cells_sim.v | while read path; do
 		arch_name=$(basename -- $arch)
-		if [ "${defines[$arch_name]}" ]; then
-			for def in ${defines[$arch_name]}; do
+		defines=""
+		if [ "$arch_name" == "ice40" ]; then
+			defines="ICE40_HX ICE40_LP ICE40_U"
+		fi
+		if [ ! -z "$defines" ]; then
+			for def in $defines; do
 				echo -n "Test $path -D$def ->"
 				iverilog -t null -I$arch -D$def $path
 				echo " ok"
