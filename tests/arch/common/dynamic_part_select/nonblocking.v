87910732f15c900cbd752158258a8af12720d277
diego <diego@symbioticeda.com>
2020-04-16 13:31:05 -0500

    Adding tests for dynamic part select optimisation

---
+++ tests/arch/common/dynamic_part_select/nonblocking.v
@@ -0,0 +1,14 @@
+module nonblocking #(parameter WIDTH=256, SELW=2)
+   (input 	           clk ,
+    input [9:0] 	   ctrl ,
+    input [15:0] 	   din ,
+    input [SELW-1:0] 	   sel ,
+    output reg [WIDTH-1:0] dout);
+
+   localparam SLICE = WIDTH/(SELW**2);
+   always @(posedge clk) begin
+      dout <= dout + 1;
+      dout[ctrl*sel+:SLICE] <= din ;
+   end
+   
+endmodule
