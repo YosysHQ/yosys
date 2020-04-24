7f203cb019c56e0eaf3387a5f7b49f266d6dd9c6
Eddie Hung <eddie@fpgeh.com>
2020-04-24 14:31:33 -0700

    tests: fsm to use a randomly-generated seed

--- tests/fsm/generate.py
+++ tests/fsm/generate.py
@@ -36,9 +36,11 @@ parser.add_argument('-S', '--seed',  type = int, help = 'seed for PRNG')
 parser.add_argument('-c', '--count', type = int, default = 50, help = 'number of test cases to generate')
 args = parser.parse_args()
 
-if args.seed is not None:
-    print("PRNG seed: %d" % args.seed)
-    random.seed(args.seed)
+seed = args.seed
+if seed is None:
+    seed = random.randrange(sys.maxsize)
+print("PRNG seed: %d" % seed)
+random.seed(seed)
 
 for idx in range(args.count):
     with open('temp/uut_%05d.v' % idx, 'w') as f:
