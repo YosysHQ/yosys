da61702655275ca2057e497db13200398fedd7d5
Alberto Gonzalez <boqwxp@airmail.cc>
2020-06-13 06:34:44 +0000

    cutpoint: Clarify control flow and replace a loop over all wires with a loop over ports only

--- passes/sat/cutpoint.cc
+++ passes/sat/cutpoint.cc
@@ -94,11 +94,11 @@ struct CutpointPass : public Pass {
 					wire->port_id = 0;
 					wire->port_input = false;
 					wire->port_output = false;
-					continue;
+				} else {
+					log("Making wire %s.%s a cutpoint.\n", log_id(module), log_id(wire));
+					for (auto bit : sigmap(wire))
+						cutpoint_bits.insert(bit);
 				}
-				log("Making wire %s.%s a cutpoint.\n", log_id(module), log_id(wire));
-				for (auto bit : sigmap(wire))
-					cutpoint_bits.insert(bit);
 			}
 
 			if (!cutpoint_bits.empty())
@@ -126,15 +126,16 @@ struct CutpointPass : public Pass {
 				}
 
 				vector<Wire*> rewrite_wires;
-				for (auto wire : module->wires()) {
-					if (!wire->port_input)
-						continue;
-					int bit_count = 0;
-					for (auto &bit : sigmap(wire))
-						if (cutpoint_bits.count(bit))
-							bit_count++;
-					if (bit_count)
-						rewrite_wires.push_back(wire);
+				for (auto id : module->ports) {
+					RTLIL::Wire *wire = module->wire(id);
+					if (wire->port_input) {
+						int bit_count = 0;
+						for (auto &bit : sigmap(wire))
+							if (cutpoint_bits.count(bit))
+								bit_count++;
+						if (bit_count)
+							rewrite_wires.push_back(wire);
+					}
 				}
 
 				for (auto wire : rewrite_wires) {
