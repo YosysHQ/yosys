c8c3c7af87804a175b3dfc60dce191b03c9741fe
whitequark <whitequark@whitequark.org>
2020-06-19 15:45:52 +0000

    Use [[maybe_unused]] instead of YS_ATTRIBUTE(unused).
    [[maybe_unused]] is available since C++17, so this commit adds
    a polyfill YS_MAYBE_UNUSED. Once we require C++17 we can drop it.

--- kernel/yosys.h
+++ kernel/yosys.h
@@ -145,6 +145,14 @@ extern Tcl_Obj *Tcl_ObjSetVar2(Tcl_Interp *interp, Tcl_Obj *part1Ptr, Tcl_Obj *p
 #endif
 
 #if __cplusplus >= 201703L
+#  define YS_MAYBE_UNUSED [[maybe_unused]];
+#elif defined(__GNUC__) || defined(__clang__)
+#  define YS_MAYBE_UNUSED __attribute__((__unused__))
+#else
+#  define YS_MAYBE_UNUSED
+#endif
+
+#if __cplusplus >= 201703L
 #  define YS_FALLTHROUGH [[fallthrough]];
 #elif defined(__clang__)
 #  define YS_FALLTHROUGH [[clang::fallthrough]];
