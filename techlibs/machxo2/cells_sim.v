bb9791a5440279321c5a1d19eb6dac1b760b670c
William D. Jones <thor0505@comcast.net>
2020-12-07 22:29:36 -0500

    machxo2: Add missing OSCH oscillator primitive.

--- techlibs/machxo2/cells_sim.v
+++ techlibs/machxo2/cells_sim.v
@@ -178,3 +178,13 @@ module FACADE_IO #(
 		end
 	endgenerate
 endmodule
+
+(* blackbox *)
+module OSCH #(
+	parameter NOM_FREQ = "2.08"
+) (
+	input STDBY,
+	output OSC,
+	output SEDSTDBY
+);
+endmodule
