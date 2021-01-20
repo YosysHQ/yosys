19fdeb6a6355bb3a851939badc3b8d3189bbdeb0
Lalit Sharma <lsharma@quicklogic.com>
2021-01-20 13:06:49 +0530

    QL: Infer FF w/o S/R in openfpga flow

--- techlibs/quicklogic/synth_quicklogic.cc
+++ techlibs/quicklogic/synth_quicklogic.cc
@@ -164,7 +164,7 @@ struct SynthQuickLogicPass : public ScriptPass {
             run(stringf("hierarchy -check %s", help_mode ? "-top <top>" : top_opt.c_str()));
         }
 
-        if (check_label("prepare")) {
+        if (check_label("prepare") && !openfpga) {
             run("proc");
             run("flatten");
             if(family == "pp3" || family == "ap") {
@@ -176,7 +176,7 @@ struct SynthQuickLogicPass : public ScriptPass {
             run("opt");
         }
 
-        if (check_label("coarse")) {
+        if (check_label("coarse") && !openfpga) {
             run("opt_expr");
             run("opt_clean");
             run("check");
@@ -198,6 +198,13 @@ struct SynthQuickLogicPass : public ScriptPass {
             run("opt -fast");
             run("memory -nomap");
             run("opt_clean");
+        }
+
+        if(openfpga) {
+            run("proc");
+            run("techmap  -D NO_LUT -map +/adff2dff.v");
+            run("synth " + top_opt + " -flatten");
+            run("clean");
         }
 
         if (check_label("map_bram", "(skip if -nobram)") && family == "pp3") {
