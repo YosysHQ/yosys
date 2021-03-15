d0180d302f8313b014193a614244e948b150e35e
Tarachand Pagarani <tpagarani@quicklogic.com>
2021-03-15 14:59:43 +0100

    QL: Add techmap for ffs needing async preset

--- techlibs/quicklogic/qlf_k4n8_cells_sim.v
+++ techlibs/quicklogic/qlf_k4n8_cells_sim.v
@@ -115,3 +115,21 @@ module sh_dff(
             Q <= D;
 endmodule
 
+(* abc9_flop, lib_whitebox *)
+module dffs(
+    output reg Q,
+    input D,
+    (* clkbuf_sink *)
+    input C,
+    input S
+);
+    parameter [0:0] INIT = 1'b0;
+    initial Q = INIT;
+
+    always @(posedge C or negedge S)
+        if (!S)
+            Q <= 1'b1;
+        else
+            Q <= D;
+endmodule
+
--- techlibs/quicklogic/qlf_k4n8_ffs_map.v
+++ techlibs/quicklogic/qlf_k4n8_ffs_map.v
@@ -21,6 +21,22 @@ module \$_DFF_PP0_ (D, Q, C, R);
     dffr _TECHMAP_REPLACE_ (.Q(Q), .D(D), .C(C), .R(!R));
 endmodule
 
+module \$_DFF_PN1_ (D, Q, C, R);
+    input D;
+    input C;
+    input R;
+    output Q;
+    dffs _TECHMAP_REPLACE_ (.Q(Q), .D(D), .C(C), .S(R));
+endmodule
+
+module \$_DFF_PP1_ (D, Q, C, R);
+    input D;
+    input C;
+    input R;
+    output Q;
+    dffs _TECHMAP_REPLACE_ (.Q(Q), .D(D), .C(C), .S(!R));
+endmodule
+
 module \$__SHREG_DFF_P_ (D, Q, C);
     input D;
     input C;
