19335d2a1b4e96bb6f5883a3a83bce563da7c2bc
Patrick Urban <patrick.urban@web.de>
2021-09-24 21:50:26 +0200

    Allow initial blocks to be disabled during tests
    Wrap initial blocks with a NO_INIT so that tests for archs without register initialization feature don't fail.

--- tests/arch/common/dffs.v
+++ tests/arch/common/dffs.v
@@ -4,9 +4,11 @@ module dff ( input d, clk, output reg q );
 endmodule
 
 module dffe( input d, clk, en, output reg q );
+`ifndef NO_INIT
     initial begin
         q = 0;
     end
+`endif
 	  always @( posedge clk )
         if ( en )
               q <= d;
