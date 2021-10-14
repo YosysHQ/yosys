6253d4ec9e782d0d36ec429fa6ac84d500edbda5
Olivier Galibert <galibert@pobox.com>
2021-10-14 16:56:10 +0200

    CycloneV: Add (passthrough) support for cyclonev_hps_interface_mpu_general_purpose

--- techlibs/intel_alm/common/megafunction_bb.v
+++ techlibs/intel_alm/common/megafunction_bb.v
@@ -697,3 +697,11 @@ output outclk;
 
 endmodule
 
+// HPS interfaces
+(* keep *)
+module cyclonev_hps_interface_mpu_general_purpose(gp_in, gp_out);
+
+input [31:0] gp_in;   
+output [31:0] gp_out;
+
+endmodule
