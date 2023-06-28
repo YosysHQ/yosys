4ffdee65e034493cf1eafd9f94d770c88d2f35e0
Charlotte <charlotte@hrzn.ee>
2023-06-28 11:51:31 +1000

    cxxrtl: store comb $print cell last EN/ARGS in module
    statics were obviously wrong -- may be multiple instantiations of any
    given module.  Extend test to cover this.

--- tests/fmt/always_comb_tb.cc
+++ tests/fmt/always_comb_tb.cc
@@ -3,11 +3,14 @@
 
 int main()
 {
-	cxxrtl_design::p_top uut;
+	cxxrtl_design::p_top uut1, uut2;
 
 	for (int i = 0; i < 20; ++i) {
-		uut.p_clk.set(!uut.p_clk);
-		uut.step();
+		uut1.p_clk.set(!uut1.p_clk);
+		uut1.step();
+
+		uut2.p_clk.set(!uut2.p_clk);
+		uut2.step();
 	}
 
 	return 0;
