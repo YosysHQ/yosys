4e94f62116cf4b9180a7a0ddb6ca4ad61a97bd53
Charlotte <charlotte@hrzn.ee>
2023-06-28 11:51:27 +1000

    simlib: blackbox `$print` cell
    It's possible to `generate` the appropriate always blocks per the
    triggers, but unlikely to be worth parsing the RTLIL \FORMAT parameter.

--- techlibs/common/simlib.v
+++ techlibs/common/simlib.v
@@ -1800,6 +1800,23 @@ end
 endmodule
 
 // --------------------------------------------------------
+
+module \$print (EN, TRG, ARGS);
+
+parameter FORMAT = "";
+parameter ARGS_WIDTH = 0;
+parameter TRG_ENABLE = 1;
+
+parameter TRG_WIDTH = 0;
+parameter TRG_POLARITY = 0;
+
+input EN;
+input [TRG_WIDTH-1:0] TRG;
+input [ARGS_WIDTH-1:0] ARGS;
+
+endmodule
+
+// --------------------------------------------------------
 `ifndef SIMLIB_NOSR
 
 module \$sr (SET, CLR, Q);
