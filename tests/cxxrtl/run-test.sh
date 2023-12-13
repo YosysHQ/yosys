1dff3c83d97724ee7d4872404b8df7201773734c
Merry <git@mary.rs>
2023-12-13 12:27:06 +0000

    tests/cxxrtl: Add -O2

--- tests/cxxrtl/run-test.sh
+++ tests/cxxrtl/run-test.sh
@@ -5,7 +5,7 @@ set -ex
 run_subtest () {
     local subtest=$1; shift
 
-    ${CC:-gcc} -std=c++11 -o cxxrtl-test-${subtest} -I../../backends/cxxrtl/runtime test_${subtest}.cc -lstdc++
+    ${CC:-gcc} -std=c++11 -O2 -o cxxrtl-test-${subtest} -I../../backends/cxxrtl/runtime test_${subtest}.cc -lstdc++
     ./cxxrtl-test-${subtest}
 }
 
