fc5ff7a265293099e25812afbbbe8e687f0b62e8
Catherine <whitequark@whitequark.org>
2024-01-19 15:54:33 +0000

    cxxrtl: always lazily format print messages.
    This is mostly useful for collecting coverage for the future `$check`
    cell, where, depending on the flavor, formatting a message may not be
    wanted even for a failed assertion.

--- tests/fmt/always_full_tb.cc
+++ tests/fmt/always_full_tb.cc
@@ -4,7 +4,7 @@ int main()
 {
 	struct : public performer {
 		int64_t vlog_time() const override { return 1; }
-		void on_print(const std::string &output, const cxxrtl::metadata_map &) override { std::cerr << output; }
+		void on_print(const lazy_fmt &output, const cxxrtl::metadata_map &) override { std::cerr << output(); }
 	} performer;
 
 	cxxrtl_design::p_always__full uut;
