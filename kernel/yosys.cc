b5b737de3887d77f117d99d3fad21a4eeb97953e
Martin Povišer <povik@cutebit.org>
2024-02-22 22:20:35 +0100

    Shrink a bit more

--- kernel/yosys.cc
+++ kernel/yosys.cc
@@ -138,11 +138,9 @@ void yosys_banner()
 {
 	log("\n");
 	log(" /----------------------------------------------------------------------------\\\n");
-	log(" |                                                                            |\n");
 	log(" |  yosys -- Yosys Open SYnthesis Suite                                       |\n");
 	log(" |  Copyright (C) 2012 - 2024  Claire Xenia Wolf <claire@yosyshq.com>         |\n");
 	log(" |  Distributed under an ISC-like license, type \"license\" to see terms        |\n");
-	log(" |                                                                            |\n");
 	log(" \\----------------------------------------------------------------------------/\n");
 	log("\n");
 	log(" %s\n", yosys_version_str);
