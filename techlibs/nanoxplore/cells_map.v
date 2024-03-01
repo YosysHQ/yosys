b4e9bb0d85f38ce81fd3eb7733e8ea35a269dc82
Lofty <dan.ravensloft@gmail.com>
2024-03-01 10:55:54 +0100

    Add FFs and related tests

--- techlibs/nanoxplore/cells_map.v
+++ techlibs/nanoxplore/cells_map.v
@@ -33,3 +33,36 @@ module \$lut (A, Y);
     end
   endgenerate
 endmodule
+
+(* techmap_celltype = "$_DFF_[NP]P[01]_" *)
+module dff(input D, C, R, output Q);
+	parameter _TECHMAP_CELLTYPE = "$_DFF_PP1_";
+  localparam dff_edge = _TECHMAP_CELLTYPE[6*8 +: 8] == "N";
+  localparam dff_type = _TECHMAP_CELLTYPE[8*8 +: 8] == "1";
+  wire _TECHMAP_REMOVEINIT_Q_ = 1'b1;
+  NX_DFF #(.dff_ctxt(1'b0), .dff_edge(dff_edge), .dff_init(1'b1), .dff_load(1'b0), .dff_sync(1'b0), .dff_type(dff_type)) _TECHMAP_REPLACE_ (.I(D), .CK(C), .L(1'b0), .R(R), .O(Q));
+endmodule
+
+(* techmap_celltype = "$_ALDFF_[NP]P_" *)
+module aldff(input D, C, L, AD, output Q);
+	parameter _TECHMAP_CELLTYPE = "$_ALDFF_PP_";
+  localparam dff_edge = _TECHMAP_CELLTYPE[8*8 +: 8] == "N";
+  wire _TECHMAP_REMOVEINIT_Q_ = 1'b1;
+  NX_DFF #(.dff_ctxt(1'b0), .dff_edge(dff_edge), .dff_init(1'b1), .dff_load(1'b1), .dff_sync(1'b0), .dff_type(2)) _TECHMAP_REPLACE_ (.I(D), .CK(C), .L(AD), .R(L), .O(Q));
+endmodule
+
+module \$_SDFF_PP0_ (input D, C, R, output Q);
+  NX_DFF #(.dff_ctxt(1'b0), .dff_edge(1'b0), .dff_init(1'b1), .dff_load(1'b0), .dff_sync(1'b1), .dff_type(1'b0)) _TECHMAP_REPLACE_ (.I(D), .CK(C), .L(1'b0), .R(R), .O(Q));
+endmodule
+
+module \$_SDFF_PP1_ (input D, C, R, output Q);
+  NX_DFF #(.dff_ctxt(1'b0), .dff_edge(1'b0), .dff_init(1'b1), .dff_load(1'b0), .dff_sync(1'b1), .dff_type(1'b1)) _TECHMAP_REPLACE_ (.I(D), .CK(C), .L(1'b0), .R(R), .O(Q));
+endmodule
+
+module \$_SDFF_NP0_ (input D, C, R, output Q);
+  NX_DFF #(.dff_ctxt(1'b0), .dff_edge(1'b1), .dff_init(1'b1), .dff_load(1'b0), .dff_sync(1'b1), .dff_type(1'b0)) _TECHMAP_REPLACE_ (.I(D), .CK(C), .L(1'b0), .R(R), .O(Q));
+endmodule
+
+module \$_SDFF_NP1_ (input D, C, R, output Q);
+  NX_DFF #(.dff_ctxt(1'b0), .dff_edge(1'b1), .dff_init(1'b1), .dff_load(1'b0), .dff_sync(1'b1), .dff_type(1'b1)) _TECHMAP_REPLACE_ (.I(D), .CK(C), .L(1'b0), .R(R), .O(Q));
+endmodule
--- techlibs/nanoxplore/cells_sim.v
+++ techlibs/nanoxplore/cells_sim.v
@@ -8,3 +8,28 @@ wire [1:0] s3 = I2 ? s2[3:2] : s2[1:0];
 assign O = I1 ? s3[1] : s3[0];
 
 endmodule
+
+module NX_DFF(input I, CK, L, R, output reg O);
+
+parameter dff_ctxt = 1'bx;
+parameter dff_edge = 1'b0;
+parameter dff_init = 1'b0;
+parameter dff_load = 1'b0;
+parameter dff_sync = 1'b0;
+parameter dff_type = 1'b0;
+
+initial begin
+	O = dff_ctxt;
+end
+
+wire clock = CK ^ dff_edge;
+wire load = (dff_type == 2) ? (dff_load ? L : 1'bx) : dff_type;
+wire async_reset = !dff_sync && dff_init && R;
+wire sync_reset = dff_sync && dff_init && R;
+
+always @(posedge clock, posedge async_reset)
+	if (async_reset) O <= load;
+	else if (sync_reset) O <= load;
+	else O <= I;
+
+endmodule
