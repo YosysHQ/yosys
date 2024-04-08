5f4d13ee3fdf49d371fe29712b95c07eba9f7f02
Martin Povišer <povik@cutebit.org>
2024-04-08 16:44:43 +0200

    techmap: Note down iteration in Kogge-Stone

--- techlibs/common/choices/kogge-stone.v
+++ techlibs/common/choices/kogge-stone.v
@@ -42,6 +42,7 @@ module _80_lcu_kogge_stone (P, G, CI, CO);
 		g[0] = g[0] | (p[0] & CI);
 
 		for (i = 0; i < $clog2(WIDTH); i = i + 1) begin
+			// iterate in reverse so we don't confuse a result from this stage and the previous
 			for (j = WIDTH - 1; j >= 2**i; j = j - 1) begin
 				g[j] = g[j] | p[j] & g[j - 2**i];
 				p[j] = p[j] & p[j - 2**i];
