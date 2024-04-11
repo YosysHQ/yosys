b21d9d296bf10b6c9e2028197231abe00ee316d5
Jannis Harder <me@jix.one>
2024-04-11 13:48:25 +0200

    ComputeGraph datatype for the upcoming functional backend

--- passes/cmds/example_dt.cc
+++ passes/cmds/example_dt.cc
@@ -1,6 +1,7 @@
 #include "kernel/yosys.h"
 #include "kernel/drivertools.h"
 #include "kernel/topo_scc.h"
+#include "kernel/functional.h"
 
 USING_YOSYS_NAMESPACE
 PRIVATE_NAMESPACE_BEGIN
@@ -38,86 +39,137 @@ struct ExampleDtPass : public Pass
 			ExampleWorker worker(module);
 			DriverMap dm;
 
+			struct ExampleFn {
+				IdString name;
+				dict<IdString, Const> parameters;
+
+				ExampleFn(IdString name) : name(name) {}
+				ExampleFn(IdString name, dict<IdString, Const> parameters) : name(name), parameters(parameters) {}
+
+				bool operator==(ExampleFn const &other) const {
+					return name == other.name && parameters == other.parameters;
+				}
+
+				unsigned int hash() const {
+					return mkhash(name.hash(), parameters.hash());
+				}
+			};
+
+			typedef ComputeGraph<ExampleFn, int, IdString, IdString> ExampleGraph;
+
+			ExampleGraph compute_graph;
+
+
 			dm.add(module);
 
 			idict<DriveSpec> queue;
 			idict<Cell *> cells;
 
 			IntGraph edges;
+			std::vector<int> graph_nodes;
 
+			auto enqueue = [&](DriveSpec const &spec) {
+				int index = queue(spec);
+				if (index == GetSize(graph_nodes))
+					graph_nodes.emplace_back(compute_graph.add(ID($pending), index).index());
+				//if (index >= GetSize(graph_nodes))
+				return compute_graph[graph_nodes[index]];
+			};
 
 			for (auto cell : module->cells()) {
 				if (cell->type.in(ID($assert), ID($assume), ID($cover), ID($check)))
-					queue(DriveBitMarker(cells(cell), 0));
+					enqueue(DriveBitMarker(cells(cell), 0));
 			}
 
 			for (auto wire : module->wires()) {
 				if (!wire->port_output)
 					continue;
-				queue(DriveChunk(DriveChunkWire(wire, 0, wire->width)));
+				enqueue(DriveChunk(DriveChunkWire(wire, 0, wire->width))).assign_key(wire->name);
 			}
-
-#define emit log
-// #define emit(X...) do {} while (false)
 
 			for (int i = 0; i != GetSize(queue); ++i)
 			{
-				emit("n%d: ", i);
 				DriveSpec spec = queue[i];
+				ExampleGraph::Ref node = compute_graph[i];
+
 				if (spec.chunks().size() > 1) {
-					emit("concat %s <-\n", log_signal(spec));
+					node.set_function(ID($$concat));
+
 					for (auto const &chunk : spec.chunks()) {
-						emit("  * %s\n", log_signal(chunk));
-						edges.add_edge(i, queue(chunk));
+						node.append_arg(enqueue(chunk));
 					}
 				} else if (spec.chunks().size() == 1) {
 					DriveChunk chunk = spec.chunks()[0];
 					if (chunk.is_wire()) {
 						DriveChunkWire wire_chunk = chunk.wire();
 						if (wire_chunk.is_whole()) {
+							node.sparse_attr() = wire_chunk.wire->name;
 							if (wire_chunk.wire->port_input) {
-								emit("input %s\n", log_signal(spec));
+								node.set_function(ExampleFn(ID($$input), {{wire_chunk.wire->name, {}}}));
 							} else {
 								DriveSpec driver = dm(DriveSpec(wire_chunk));
-								edges.add_edge(i, queue(driver));
-								emit("wire driver %s <- %s\n", log_signal(spec), log_signal(driver));
+								node.set_function(ID($$buf));
+
+								node.append_arg(enqueue(driver));
 							}
 						} else {
 							DriveChunkWire whole_wire(wire_chunk.wire, 0, wire_chunk.width);
-							edges.add_edge(i, queue(whole_wire));
-							emit("wire slice %s <- %s\n", log_signal(spec), log_signal(DriveSpec(whole_wire)));
+							node.set_function(ExampleFn(ID($$slice), {{ID(offset), wire_chunk.offset}, {ID(width), wire_chunk.width}}));
+							node.append_arg(enqueue(whole_wire));
 						}
 					} else if (chunk.is_port()) {
 						DriveChunkPort port_chunk = chunk.port();
 						if (port_chunk.is_whole()) {
 							if (dm.celltypes.cell_output(port_chunk.cell->type, port_chunk.port)) {
-								int cell_marker = queue(DriveBitMarker(cells(port_chunk.cell), 0));
-								if (!port_chunk.cell->type.in(ID($dff), ID($ff)))
-									edges.add_edge(i, cell_marker);
-								emit("cell output %s %s\n", log_id(port_chunk.cell), log_id(port_chunk.port));
+								if (port_chunk.cell->type.in(ID($dff), ID($ff)))
+								{
+									Cell *cell = port_chunk.cell;
+									node.set_function(ExampleFn(ID($$state), {{cell->name, {}}}));
+									for (auto const &conn : cell->connections()) {
+										if (!dm.celltypes.cell_input(cell->type, conn.first))
+											continue;
+										enqueue(DriveChunkPort(cell, conn)).assign_key(cell->name);
+									}
+								}
+								else
+								{
+									node.set_function(ExampleFn(ID($$cell_output), {{port_chunk.port, {}}}));
+									node.append_arg(enqueue(DriveBitMarker(cells(port_chunk.cell), 0)));
+								}
 							} else {
+								node.set_function(ID($$buf));
+
 								DriveSpec driver = dm(DriveSpec(port_chunk));
-								edges.add_edge(i, queue(driver));
-								emit("cell port driver %s <- %s\n", log_signal(spec), log_signal(driver));
+								node.append_arg(enqueue(driver));
 							}
 
 						} else {
 							DriveChunkPort whole_port(port_chunk.cell, port_chunk.port, 0, GetSize(port_chunk.cell->connections().at(port_chunk.port)));
-							edges.add_edge(i, queue(whole_port));
-							emit("port slice %s <- %s\n", log_signal(spec), log_signal(DriveSpec(whole_port)));
+							node.set_function(ID($$buf));
+							node.append_arg(enqueue(whole_port));
 						}
 					} else if (chunk.is_constant()) {
-						emit("constant %s <- %s\n", log_signal(spec), log_const(chunk.constant()));
+						node.set_function(ExampleFn(ID($$const), {{ID(value), chunk.constant()}}));
+
+					} else if (chunk.is_multiple()) {
+						node.set_function(ID($$multi));
+						for (auto const &driver : chunk.multiple().multiple())
+							node.append_arg(enqueue(driver));
 					} else if (chunk.is_marker()) {
 						Cell *cell = cells[chunk.marker().marker];
-						emit("cell %s %s\n", log_id(cell->type), log_id(cell));
+
+						node.set_function(ExampleFn(cell->type, cell->parameters));
 						for (auto const &conn : cell->connections()) {
 							if (!dm.celltypes.cell_input(cell->type, conn.first))
 								continue;
-							emit("  * %s <- %s\n", log_id(conn.first), log_signal(conn.second));
-							edges.add_edge(i, queue(DriveChunkPort(cell, conn)));
+
+							node.append_arg(enqueue(DriveChunkPort(cell, conn)));
 						}
+					} else if (chunk.is_none()) {
+						node.set_function(ID($$undriven));
+
 					} else {
+						log_error("unhandled drivespec: %s\n", log_signal(chunk));
 						log_abort();
 					}
 				} else {
@@ -125,13 +177,75 @@ struct ExampleDtPass : public Pass
 				}
 			}
 
-			topo_sorted_sccs(edges, [&](int *begin, int *end) {
-				emit("scc:");
-				for (int *i = begin; i != end; ++i)
-					emit(" n%d", *i);
-				emit("\n");
-			});
 
+			// Perform topo sort and detect SCCs
+			ExampleGraph::SccAdaptor compute_graph_scc(compute_graph);
+
+
+			std::vector<int> perm;
+			topo_sorted_sccs(compute_graph_scc, [&](int *begin, int *end) {
+				perm.insert(perm.end(), begin, end);
+				if (end > begin + 1)
+				{
+					log_warning("SCC:");
+					for (int *i = begin; i != end; ++i)
+						log(" %d", *i);
+					log("\n");
+				}
+			}, /* sources_first */ true);
+			compute_graph.permute(perm);
+
+
+			// Forward $$buf unless we have a name in the sparse attribute
+			std::vector<int> alias;
+			perm.clear();
+
+			for (int i = 0; i < compute_graph.size(); ++i)
+			{
+				if (compute_graph[i].function().name == ID($$buf) && !compute_graph[i].has_sparse_attr() && compute_graph[i].arg(0).index() < i)
+				{
+
+					alias.push_back(alias[compute_graph[i].arg(0).index()]);
+				}
+				else
+				{
+					alias.push_back(GetSize(perm));
+					perm.push_back(i);
+				}
+			}
+			compute_graph.permute(perm, alias);
+
+			// Dump the compute graph
+			for (int i = 0; i < compute_graph.size(); ++i)
+			{
+				auto ref = compute_graph[i];
+				log("n%d ", i);
+				log("%s", log_id(ref.function().name));
+				for (auto const &param : ref.function().parameters)
+				{
+					if (param.second.empty())
+						log("[%s]", log_id(param.first));
+					else
+						log("[%s=%s]", log_id(param.first), log_const(param.second));
+				}
+				log("(");
+
+				for (int i = 0, end = ref.size(); i != end; ++i)
+				{
+					if (i > 0)
+						log(", ");
+					log("n%d", ref.arg(i).index());
+				}
+				log(")\n");
+				if (ref.has_sparse_attr())
+					log("// wire %s\n", log_id(ref.sparse_attr()));
+				log("// was #%d %s\n", ref.attr(), log_signal(queue[ref.attr()]));
+			}
+
+			for (auto const &key : compute_graph.keys())
+			{
+				log("return %d as %s \n", key.second, log_id(key.first));
+			}
 		}
 		log("Plugin test passed!\n");
 	}
