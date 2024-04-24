606439b44c417773453694e69a940410e0bebe1a
Miodrag Milanovic <mmicko@gmail.com>
2024-04-24 11:28:03 +0200

    do not leave NX_RAM empty to prevent removing it

--- techlibs/nanoxplore/brams_map.v
+++ techlibs/nanoxplore/brams_map.v
@@ -29,4 +29,16 @@ input [12:0] PORT_B_ADDR;
 input [PORT_B_WIDTH-1:0] PORT_B_WR_DATA;
 output [PORT_B_WIDTH-1:0] PORT_B_RD_DATA;
 
+NX_RAM_WRAP #(
+) _TECHMAP_REPLACE_ (
+    .ACK(PORT_A_CLK),
+    .AA(PORT_A_ADDR),
+    .AI(PORT_A_WR_DATA),
+    .AO(PORT_A_RD_DATA),
+
+    .BCK(PORT_B_CLK),
+    .BA(PORT_B_ADDR),
+    .BI(PORT_B_WR_DATA),
+    .BO(PORT_B_RD_DATA)
+);
 endmodule
