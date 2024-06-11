b9b776d211e7e42d0254920b5a86c414cdaac940
Akash Levy <akashlevy@gmail.com>
2024-06-10 20:33:05 -0700

    Update for no preservation of user nets

--- frontends/verific/verific.cc
+++ frontends/verific/verific.cc
@@ -3235,7 +3235,7 @@ struct VerificPass : public Pass {
 			Message::RegisterCallBackMsg(msg_func);
 
 			RuntimeFlags::SetVar("db_preserve_user_instances", 1);
-			RuntimeFlags::SetVar("db_preserve_user_nets", 1);
+			// RuntimeFlags::SetVar("db_preserve_user_nets", 1); // SILIMATE: comment out to improve optimization
 			RuntimeFlags::SetVar("db_preserve_x", 1);
 
 			RuntimeFlags::SetVar("db_merge_cascaded_muxes", 1); // SILIMATE: add to improve optimization
