7fe9157df2f38c871cdb912cd2150ea701574358
Krystine Sherwin <93062060+KrystalDelusion@users.noreply.github.com>
2024-08-23 09:05:44 +1200

    smtr: Add rkt to functional tests

--- tests/functional/test_functional.py
+++ tests/functional/test_functional.py
@@ -72,6 +72,19 @@ def test_smt(cell, parameters, tmp_path, num_steps, rnd):
     smt_vcd.simulate_smt(smt_file, vcd_functional_file, num_steps, rnd(cell.name + "-smt"))
     yosys_sim(rtlil_file, vcd_functional_file, vcd_yosys_sim_file, getattr(cell, 'sim_preprocessing', ''))
 
+def test_rkt(cell, parameters, tmp_path, num_steps, rnd):
+    import rkt_vcd
+
+    rtlil_file = tmp_path / 'rtlil.il'
+    rkt_file = tmp_path / 'smtlib.rkt'
+    vcd_functional_file = tmp_path / 'functional.vcd'
+    vcd_yosys_sim_file = tmp_path / 'yosys.vcd'
+
+    cell.write_rtlil_file(rtlil_file, parameters)
+    yosys(f"read_rtlil {quote(rtlil_file)} ; clk2fflogic ; write_functional_rosette -provides {quote(rkt_file)}")
+    rkt_vcd.simulate_rosette(rkt_file, vcd_functional_file, num_steps, rnd(cell.name + "-rkt"))
+    yosys_sim(rtlil_file, vcd_functional_file, vcd_yosys_sim_file, getattr(cell, 'sim_preprocessing', ''))
+
 def test_print_graph(tmp_path):
     tb_file = base_path / 'tests/functional/picorv32_tb.v'
     cpu_file = base_path / 'tests/functional/picorv32.v'
