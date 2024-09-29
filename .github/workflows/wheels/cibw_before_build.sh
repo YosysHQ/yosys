67f17a1c97a097eb59d8a7d1b81fc5aff66f0da8
Mohamed Gaber <donn@efabless.com>
2024-09-29 12:28:18 +0300

    wheels: symlink python3-config

--- .github/workflows/wheels/cibw_before_build.sh
+++ .github/workflows/wheels/cibw_before_build.sh
@@ -5,6 +5,10 @@ set -x
 make clean
 
 # DEBUG: show python3 and python3-config outputs
+if [ "$(uname)" != "Linux" ]; then
+    # https://github.com/pypa/cibuildwheel/issues/2021
+    ln -s $(dirname $(readlink -f $(which python3)))/python3-config $(dirname $(which python3))/python3-config
+fi
 python3 --version
 python3-config --includes
 
