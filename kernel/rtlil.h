1be0a501854d66b7b5937558a407d817d659b108
Akash Levy <akash@silimate.com>
2024-10-15 03:20:54 -0700

    Fix comma that pyosys hates

--- kernel/rtlil.h
+++ kernel/rtlil.h
@@ -53,7 +53,7 @@ namespace RTLIL
 		CONST_FLAG_NONE   = 0,
 		CONST_FLAG_STRING = 1,
 		CONST_FLAG_SIGNED = 2,  // only used for parameters
-		CONST_FLAG_REAL   = 4,  // only used for parameters
+		CONST_FLAG_REAL   = 4   // only used for parameters
 	};
 
 	// // Union discriminator. Values are exclusive
