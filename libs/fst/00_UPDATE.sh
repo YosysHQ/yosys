2edb9397c317cb3ef3e4d29741069e0cd900eadb
Krystine Sherwin <93062060+KrystalDelusion@users.noreply.github.com>
2024-10-17 07:05:23 +1300

    libs/fst: Update from upstream
    Add shell script (based on minisat lib) to clone and copy relevant files.
    Unclear if there are any changes lost that we need to patch back in.

---
+++ libs/fst/00_UPDATE.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mv config.h config.h.bak
+rm -f *.txt *.cc *.h
+git clone --depth 1 https://github.com/gtkwave/gtkwave fst_upstream
+rm fst_upstream/lib/libfst/CMakeLists.txt
+mv fst_upstream/lib/libfst/*.{h,c,txt} .
+rm -rf fst_upstream
+
+for src in *.c; do
+    mv -- "$src" "${src%.c}.cc"
+done
+mv config.h.bak config.h
