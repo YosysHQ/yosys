31907718928d49a2c504097498870703f631dae3
Akash Levy <akash@silimate.com>
2024-11-16 22:21:58 -0800

    Small fixes to get a test working

--- tests/various/README.md
+++ tests/various/README.md
@@ -9,7 +9,3 @@
 - `check4`: we handle logic loops differently
 - `ice40_mince_abc9`: we do not support `abc9`
 - `plugin`: we do not support plugins
-
-## Broken
-
-- `logger_cmd_error`: not sure why broken...
--- tests/various/logger_cmd_error.sh
+++ tests/various/logger_cmd_error.sh
@@ -2,8 +2,7 @@
 
 trap 'echo "ERROR in logger_cmd_error.sh" >&2; exit 1' ERR
 
-(../../yosys -v 3 -C <<EOF
-yosys -import
+(../../yosys -v 3 -p '
 hierarchy -top nonexistent
-EOF
+'
 ) 2>&1 | grep -F "ERROR: Module \`nonexistent' not found!" > /dev/null
