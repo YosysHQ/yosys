bf8aece4e4ff5c5e34fe56f1d1575d1c5a9ad85f
Mike Inouye <mikeinouye@google.com>
2025-04-23 18:40:35 +0000

    Add test to verify that the liberty format is properly parsed.

--- tests/liberty/run-test.sh
+++ tests/liberty/run-test.sh
@@ -7,6 +7,10 @@ for x in *.lib; do
 	../../yosys-filterlib - $x 2>/dev/null > $x.filtered
 	../../yosys-filterlib -verilogsim $x > $x.verilogsim
 	diff $x.filtered $x.filtered.ok && diff $x.verilogsim $x.verilogsim.ok
+	if [[ -e ${x%.lib}.log.ok ]]; then
+		../../yosys -p "dfflibmap -info -liberty ${x}" -TqqQl ${x%.lib}.log
+		diff ${x%.lib}.log ${x%.lib}.log.ok
+	fi
 done || exit 1
 
 for x in *.ys; do
