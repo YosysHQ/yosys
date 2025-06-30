b6e5bf13245241fb129f27226f909c41ac479196
Nick Allison <99363282+nickrallison@users.noreply.github.com>
2025-06-30 15:13:44 -0600

    CMake: Rebase From Main & Tests Working

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# This script builds the project using CMake in a way that is compatible with
+# the existing Makefile build system.
+
+# 1. Initialize the submodules
+git submodule update --init --recursive
+
+# 2. Build the project
+mkdir -p build
+cmake -B build
+cmake --build build --parallel $(nproc)
+
+# 3. Install the project into test install dir
+cmake --install build --prefix .
+
+# 4. Test the project
+# ctest -j$(nproc) --test-dir build --output-on-failure
+# Or Rerun Failed Tests
+# ctest -j$(nproc) --test-dir build --rerun-failed --output-on-failure
