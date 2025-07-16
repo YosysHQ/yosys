80517243c93dea3777d60f0882f28eef09332d8f
Nick Allison <99363282+nickrallison@users.noreply.github.com>
2025-07-16 13:58:17 -0600

    CMake: Rename Object Library for Ninja Build & Changed install location

--- scripts/debug_build.sh
+++ scripts/debug_build.sh
@@ -8,11 +8,11 @@ git submodule update --init --recursive
 
 # 2. Build the project
 mkdir -p build
-cmake -B build -DCMAKE_BUILD_TYPE=Debug -D ENABLE_CCACHE:BOOL=ON -D CMAKE_INSTALL_PREFIX:PATH=/usr/local
+cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Debug -D ENABLE_CCACHE:BOOL=ON -D CMAKE_INSTALL_PREFIX:PATH=/usr/local
 cmake --build build --parallel $(nproc)
 
 # 3. Install the project into test install dir
-cmake --install build --prefix .
+# cmake --install build --prefix .
 
 # 4. Test the project
 # ctest -j$(nproc) --test-dir build --output-on-failure
