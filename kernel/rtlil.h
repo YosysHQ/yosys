b394629e3fa52a306b80ba05ca2b388503d0ab60
Jannis Harder <me@jix.one>
2025-08-14 16:13:43 +0200

    kernel: Add RTLIL::PortDir for a combined input and output flag

--- kernel/rtlil.h
+++ kernel/rtlil.h
@@ -83,6 +83,13 @@ namespace RTLIL
 		SB_EXCL_BB_CMDERR = 15 // call log_cmd_error on black boxed module
 	};
 
+	enum PortDir : unsigned char  {
+		PD_UNKNOWN = 0,
+		PD_INPUT = 1,
+		PD_OUTPUT = 2,
+		PD_INOUT = 3
+	};
+
 	struct Const;
 	struct AttrObject;
 	struct NamedObject;
@@ -1943,6 +1950,7 @@ public:
 	bool known() const;
 	bool input(const RTLIL::IdString &portname) const;
 	bool output(const RTLIL::IdString &portname) const;
+	PortDir port_dir(const RTLIL::IdString &portname) const;
 
 	// access cell parameters
 	bool hasParam(const RTLIL::IdString &paramname) const;
