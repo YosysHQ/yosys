3ca2b7951fb35f1714a4b2dbb69f8e199ea94997
Krystine Sherwin <93062060+KrystalDelusion@users.noreply.github.com>
2025-08-19 17:05:51 +1200

    memlib: Fix ubsan

--- passes/memory/memlib.h
+++ passes/memory/memlib.h
@@ -109,7 +109,7 @@ struct PortVariant {
 	PortKind kind;
 	int clk_shared;
 	ClkPolKind clk_pol;
-	bool clk_en;
+	bool clk_en = false;
 	bool width_tied;
 	int min_wr_wide_log2;
 	int max_wr_wide_log2;
