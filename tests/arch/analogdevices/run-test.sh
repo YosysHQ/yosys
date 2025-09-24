bdf767e65eb2c47f89dc41a7de2616aab11167c1
Lofty <dan.ravensloft@gmail.com>
2025-09-24 20:56:27 +0100

    test suite

---
+++ tests/arch/analogdevices/run-test.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -eu
+source ../../gen-tests-makefile.sh
+generate_mk --yosys-scripts --bash --yosys-args "-w 'Yosys has only limited support for tri-state logic at the moment.'"
