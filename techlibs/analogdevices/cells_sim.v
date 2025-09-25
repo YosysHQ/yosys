16906789110503735d6154a8022568aafc8d11b1
Lofty <dan.ravensloft@gmail.com>
2025-09-25 15:09:16 +0100

    analogdevices: remove some extra cells!

--- techlibs/analogdevices/cells_sim.v
+++ techlibs/analogdevices/cells_sim.v
@@ -1347,48 +1347,6 @@ endmodule
 
 // Dual port.
 
-module RAM16X1D (
-  output DPO, SPO,
-  input  D,
-  (* clkbuf_sink *)
-  (* invertible_pin = "IS_WCLK_INVERTED" *)
-  input  WCLK,
-  input  WE,
-  input  A0, A1, A2, A3,
-  input  DPRA0, DPRA1, DPRA2, DPRA3
-);
-  parameter INIT = 16'h0;
-  parameter IS_WCLK_INVERTED = 1'b0;
-  wire [3:0] a = {A3, A2, A1, A0};
-  wire [3:0] dpra = {DPRA3, DPRA2, DPRA1, DPRA0};
-  reg [15:0] mem = INIT;
-  assign SPO = mem[a];
-  assign DPO = mem[dpra];
-  wire clk = WCLK ^ IS_WCLK_INVERTED;
-  always @(posedge clk) if (WE) mem[a] <= D;
-endmodule
-
-module RAM16X1D_1 (
-  output DPO, SPO,
-  input  D,
-  (* clkbuf_sink *)
-  (* invertible_pin = "IS_WCLK_INVERTED" *)
-  input  WCLK,
-  input  WE,
-  input  A0, A1, A2, A3,
-  input  DPRA0, DPRA1, DPRA2, DPRA3
-);
-  parameter INIT = 16'h0;
-  parameter IS_WCLK_INVERTED = 1'b0;
-  wire [3:0] a = {A3, A2, A1, A0};
-  wire [3:0] dpra = {DPRA3, DPRA2, DPRA1, DPRA0};
-  reg [15:0] mem = INIT;
-  assign SPO = mem[a];
-  assign DPO = mem[dpra];
-  wire clk = WCLK ^ IS_WCLK_INVERTED;
-  always @(negedge clk) if (WE) mem[a] <= D;
-endmodule
-
 (* abc9_box, lib_whitebox *)
 module RAM32X1D (
   output DPO, SPO,
