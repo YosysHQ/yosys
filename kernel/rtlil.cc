51aea12b4cd74556ed555486fac951326e8f9242
Robert O'Callahan <rocallahan@google.com>
2025-10-09 23:28:10 +0000

    Create RTLIL::OwningIdString and use it in a few places

--- kernel/rtlil.cc
+++ kernel/rtlil.cc
@@ -42,7 +42,7 @@ std::vector<uint32_t> RTLIL::IdString::global_refcount_storage_;
 std::vector<int> RTLIL::IdString::global_free_idx_list_;
 #endif
 
-#define X(_id) const RTLIL::IdString RTLIL::IDInternal::_id(RTLIL::StaticId::_id);
+#define X(_id) const RTLIL::OwningIdString RTLIL::IDInternal::_id(RTLIL::StaticId::_id);
 #include "kernel/constids.inc"
 #undef X
 
