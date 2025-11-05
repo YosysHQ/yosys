4c903fe7f3d6f4038d7c6b915a3eb525712c1cd8
Akash Levy <akash@silimate.com>
2025-11-04 22:00:26 -0800

    Use upstream implementation of logger_cmd_error.sh in tests/various

--- tests/various/logger_cmd_error.sh
+++ tests/various/logger_cmd_error.sh
@@ -2,7 +2,8 @@
 
 trap 'echo "ERROR in logger_cmd_error.sh" >&2; exit 1' ERR
 
-(../../yosys -v 3 -p '
+(../../yosys -v 3 -C <<EOF
+yosys -import
 hierarchy -top nonexistent
-'
+EOF
 ) 2>&1 | grep -F "ERROR: Module \`nonexistent' not found!" > /dev/null
