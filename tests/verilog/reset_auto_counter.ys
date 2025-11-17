YosysHQ/yosys

commit c4c389fdd71f9dfc653d64789437084fd091f21e

Fix verilog backend to avoid IdString::c_str()

backends/verilog/verilog_backend.cc
@@ -108,22 +108,30 @@ IdString initial_id;
 
 void reset_auto_counter_id(RTLIL::IdString id, bool may_rename)
 {
-	const char *str = id.c_str();
-
-	if (*str == '$' && may_rename && !norename)
-		auto_name_map[id] = auto_name_counter++;
-
-	if (str[0] != '\\' || str[1] != '_' || str[2] == 0)
+	auto it = id.begin();
+	auto it_end = id.end();
+	if (it == it_end)
 		return;
 
-	for (int i = 2; str[i] != 0; i++) {
-		if (str[i] == '_' && str[i+1] == 0)
+	if (*it == '$' && may_rename && !norename)
+		auto_name_map[id] = auto_name_counter++;
+
+	if (*it != '\\' || *it != '_' || (it + 1) == it_end)
+		return;
+
+	it += 2;
+	auto start = it;
+	while (it != it_end) {
+		char ch = *it;
+		if (ch == '_' && (it + 1) == it_end)
 			continue;
-		if (str[i] < '0' || str[i] > '9')
+		if (ch < '0' || ch > '9')
 			return;
 	}
 
-	int num = atoi(str+2);
+	std::string s;
+	std::copy(start, it_end, std::back_inserter(s));
+	int num = atoi(s.c_str());
 	if (num >= auto_name_offset)
 		auto_name_offset = num + 1;
 }

commit b870693393db0842ed5b00623809397fbad3fc7c

Fix reset_auto_counter_id to correctly detect _NNN_ patterns
This fixes a regression caused by commit c4c389fdd71f9dfc653d64789437084fd091f21e.

backends/verilog/verilog_backend.cc
@@ -116,21 +116,21 @@ void reset_auto_counter_id(RTLIL::IdString id, bool may_rename)
 	if (*it == '$' && may_rename && !norename)
 		auto_name_map[id] = auto_name_counter++;
 
-	if (*it != '\\' || *it != '_' || (it + 1) == it_end)
+	if (*it != '\\' || (it + 1) == it_end || *(it + 1) != '_' || (it + 2) == it_end)
 		return;
 
+	std::string s;
 	it += 2;
-	auto start = it;
 	while (it != it_end) {
 		char ch = *it;
 		if (ch == '_' && (it + 1) == it_end)
-			continue;
+			break;
 		if (ch < '0' || ch > '9')
 			return;
+		s.push_back(ch);
+		++it;
 	}
 
-	std::string s;
-	std::copy(start, it_end, std::back_inserter(s));
 	int num = atoi(s.c_str());
 	if (num >= auto_name_offset)
 		auto_name_offset = num + 1;

tests/verilog/reset_auto_counter.ys
@@ -1,0 +1,17 @@
+read_verilog -sv <<EOT
+module arithmetic (
+    input  logic [7:0] _0_,
+    input  logic [7:0] _1_,
+    output logic [7:0] _2_,
+);
+  assign _2_ = _0_ + _1_;
+
+endmodule : arithmetic
+EOT
+
+hierarchy
+techmap
+write_verilog reset_auto_counter.v
+! ! grep -qE '_0+0_' reset_auto_counter.v
+! ! grep -qE '_0+1_' reset_auto_counter.v
+! ! grep -qE '_0+2_' reset_auto_counter.v