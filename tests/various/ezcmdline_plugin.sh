bd9dbea4eac201c4fee2e93785e357c1b837c5e3
Gus Smith <guscomps@gmail.com>
2026-01-20 10:07:44 -0800

    Add -I

--- tests/various/ezcmdline_plugin.sh
+++ tests/various/ezcmdline_plugin.sh
@@ -4,5 +4,5 @@ DIR=$(cd "$(dirname "$0")" && pwd)
 BASEDIR=$(cd "$DIR/../.." && pwd)
 rm -f "$DIR/ezcmdline_plugin.so"
 chmod +x "$DIR/ezcmdline_dummy_solver"
-"$BASEDIR/yosys-config" --build "$DIR/ezcmdline_plugin.so" "$DIR/ezcmdline_plugin.cc"
+"$BASEDIR/yosys-config" --build "$DIR/ezcmdline_plugin.so" "$DIR/ezcmdline_plugin.cc" -I"$BASEDIR"
 "$BASEDIR/yosys" -m "$DIR/ezcmdline_plugin.so" -p "ezcmdline_test -cmd $DIR/ezcmdline_dummy_solver" | grep -q "ezcmdline_test passed!"
