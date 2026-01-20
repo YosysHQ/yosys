9ed56ac72c3d391119174a9f9029a5b71caf8e70
Gus Smith <guscomps@gmail.com>
2026-01-20 10:44:47 -0800

    Mimic pattern of how other tests build plugins
    Seems like using --build isn't supported in CI

--- tests/various/ezcmdline_plugin.sh
+++ tests/various/ezcmdline_plugin.sh
@@ -4,5 +4,9 @@ DIR=$(cd "$(dirname "$0")" && pwd)
 BASEDIR=$(cd "$DIR/../.." && pwd)
 rm -f "$DIR/ezcmdline_plugin.so"
 chmod +x "$DIR/ezcmdline_dummy_solver"
-"$BASEDIR/yosys-config" --build "$DIR/ezcmdline_plugin.so" "$DIR/ezcmdline_plugin.cc" -I"$BASEDIR"
+CXXFLAGS=$("$BASEDIR/yosys-config" --cxxflags)
+DATDIR=$("$BASEDIR/yosys-config" --datdir)
+DATDIR=${DATDIR//\//\\\/}
+CXXFLAGS=${CXXFLAGS//$DATDIR/..\/..\/share}
+"$BASEDIR/yosys-config" --exec --cxx ${CXXFLAGS} -I"$BASEDIR" --ldflags -shared -o "$DIR/ezcmdline_plugin.so" "$DIR/ezcmdline_plugin.cc"
 "$BASEDIR/yosys" -m "$DIR/ezcmdline_plugin.so" -p "ezcmdline_test -cmd $DIR/ezcmdline_dummy_solver" | grep -q "ezcmdline_test passed!"
