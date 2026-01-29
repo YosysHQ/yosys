7439d2489e4e5bfedd987d0a7a306955cf451bf7
Natalia <nataliakokoromyti@gmail.com>
2026-01-29 02:20:50 -0800

    add assertion to run_pass test

--- tests/pyosys/test_design_run_pass.py
+++ tests/pyosys/test_design_run_pass.py
@@ -3,10 +3,11 @@ from pathlib import Path
 from pyosys import libyosys as ys
 
 __file_dir__ = Path(__file__).absolute().parent
+src = __file_dir__.parent / "simple" / "fiedler-cooley.v"
 
 design = ys.Design()
-design.run_pass(
-	["read_verilog", str(__file_dir__.parent / "simple" / "fiedler-cooley.v")]
-)
-design.run_pass("prep")
-design.run_pass(["opt", "-full"])
+design.run_pass(["read_verilog", str(src)])
+design.run_pass("hierarchy -top up3down5")
+design.run_pass(["proc"])
+design.run_pass("opt -full")
+design.run_pass("select -assert-mod-count 1 up3down5")
