c4094e457b5013d64c60342444c7cd204382c3c7
Emil J. Tywoniak <emil@tywoniak.eu>
2026-02-11 11:34:54 +0100

    abc9: remove -genlib, -constr

--- passes/techmap/abc9.cc
+++ passes/techmap/abc9.cc
@@ -219,9 +219,8 @@ struct Abc9Pass : public ScriptPass
 		for (argidx = 1; argidx < args.size(); argidx++) {
 			std::string arg = args[argidx];
 			if ((arg == "-exe" || arg == "-script" || arg == "-D" ||
-						/*arg == "-S" ||*/ arg == "-lut" || arg == "-luts" ||
-						/*arg == "-box" ||*/ arg == "-W" || arg == "-genlib" ||
-						arg == "-constr" || arg == "-dont_use") &&
+						arg == "-lut" || arg == "-luts" ||
+						arg == "-W" || arg == "-dont_use") &&
 					argidx+1 < args.size()) {
 				if (arg == "-lut" || arg == "-luts")
 					lut_mode = true;
