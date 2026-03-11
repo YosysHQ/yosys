486c3715fb5b39b18c1cac0a06939273317f4072
Miodrag Milanovic <mmicko@gmail.com>
2026-03-11 08:02:11 +0100

    Enabled realmath that was disabled for some reason

--- tests/realmath/run-test.sh
+++ tests/realmath/run-test.sh
@@ -23,8 +23,8 @@ echo "running tests.."
 for ((i = 0; i < $count; i++)); do
 	echo -n "[$i]"
 	idx=$( printf "%05d" $i )
-	../../../yosys -qq uut_${idx}.ys
-	iverilog -o uut_${idx}_tb uut_${idx}_tb.v uut_${idx}.v uut_${idx}_syn.v
+	../../../yosys -qq uut_${idx}.ys >/dev/null 2>&1
+	iverilog -o uut_${idx}_tb uut_${idx}_tb.v uut_${idx}.v uut_${idx}_syn.v >/dev/null 2>&1
 	./uut_${idx}_tb | tee uut_${idx}.err
 	if test -s uut_${idx}.err; then
 		echo "Note: Make sure that 'iverilog' is an up-to-date git checkout of Icarus Verilog."
