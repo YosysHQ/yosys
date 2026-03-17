a4398d4d90a42fd34244bd1d33248d33fdf185fc
Emil J. Tywoniak <emil@tywoniak.eu>
2026-03-17 23:00:22 +0100

    fixup! cxxrtl: ignore $input_port

--- backends/cxxrtl/cxxrtl_backend.cc
+++ backends/cxxrtl/cxxrtl_backend.cc
@@ -1532,7 +1532,8 @@ struct CxxrtlWorker {
 				f << (cell->getParam(ID::CLR_POLARITY).as_bool() ? "" : ".bit_not()") << ");\n";
 			}
 		// Internal cells
-		} else if (is_internal_cell(cell->type) && cell->type != ID($input_port)) {
+		} else if (cell->type == ID($input_port)) {
+		} else if (is_internal_cell(cell->type)) {
 			log_cmd_error("Unsupported internal cell `%s'.\n", cell->type);
 		// User cells
 		} else if (for_debug) {
