4ff4af73893b5e26a4688bbb8fd4e0292bb75009
Miodrag Milanovic <mmicko@gmail.com>
2026-03-20 15:12:51 +0100

    Try fixing tests

--- tests/blif/generate_mk.py
+++ tests/blif/generate_mk.py
@@ -6,6 +6,6 @@ sys.path.append("..")
 import gen_tests_makefile
 
 gen_tests_makefile.generate(["--yosys-scripts"], [],
-"""if [ -f $(@:.ys=.blif) ]; then \\
-    diff <(tail -n +2 $(@:.ys=.blif).out) <(tail -n +2 $(@:.ys=.blif).ok) >/dev/null 2>&1; \\
-fi""")
+"""[ ! -f $(@:.ys=.blif) ] || \\
+    diff <(tail -n +2 $(@:.ys=.blif).out) <(tail -n +2 $(@:.ys=.blif).ok) >/dev/null 2>&1 || exit 1; \\
+""")
