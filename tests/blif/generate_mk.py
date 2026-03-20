0297eabeb3a5179802bffe6df74656132b88699b
Miodrag Milanovic <mmicko@gmail.com>
2026-03-20 16:16:58 +0100

    Make test simple

--- tests/blif/generate_mk.py
+++ tests/blif/generate_mk.py
@@ -5,7 +5,4 @@ sys.path.append("..")
 
 import gen_tests_makefile
 
-gen_tests_makefile.generate(["--yosys-scripts"], [],
-"""[ ! -f $(@:.ys=.blif) ] || \\
-    sed '1d' $(@:.ys=.blif).out | diff - <(sed '1d' $(@:.ys=.blif).ok) >/dev/null 2>&1 || exit 1; \\
-""")
+gen_tests_makefile.generate(["--yosys-scripts"])
