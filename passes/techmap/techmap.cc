c7e56da381b65b7565d140b0042bd39acc496753
Emil J. Tywoniak <emil@tywoniak.eu>
2026-04-02 11:40:33 +0200

    Revert "techmap: call hierarchy on map files to determine port directions"
    This reverts commit eabbf6d22527f386811c7b507f74f887a258b397.

--- passes/techmap/techmap.cc
+++ passes/techmap/techmap.cc
@@ -1244,7 +1244,6 @@ struct TechmapPass : public Pass {
 					Frontend::frontend_call(map, nullptr, fn, (fn.size() > 3 && fn.compare(fn.size()-3, std::string::npos, ".il") == 0 ? "rtlil" : verilog_frontend));
 				}
 		}
-		Pass::call(map, "hierarchy");
 
 		log_header(design, "Continuing TECHMAP pass.\n");
 
