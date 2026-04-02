35263298db2c523419c498c770027f48acbdbf50
Emil J. Tywoniak <emil@tywoniak.eu>
2026-04-02 17:01:09 +0200

    rtlil_bufnorm: ignore timing info harder

--- kernel/rtlil_bufnorm.cc
+++ kernel/rtlil_bufnorm.cc
@@ -1059,10 +1059,15 @@ void RTLIL::Cell::unsetPort(RTLIL::IdString portname)
 	}
 }
 
+static bool ignored_cell(const RTLIL::IdString& type)
+{
+	return type == ID($specify2) || type == ID($specify3) || type == ID($specrule);
+}
+
 void RTLIL::Cell::setPort(RTLIL::IdString portname, RTLIL::SigSpec signal)
 {
 	bool is_input_port = false;
-	if (module->sig_norm_index != nullptr && type != ID($specify2) && type != ID($specify3) && type != ID($specrule)) {
+	if (module->sig_norm_index != nullptr && !ignored_cell(type)) {
 		module->sig_norm_index->sigmap.apply(signal);
 		auto dir = port_dir(portname);
 
@@ -1098,7 +1103,7 @@ void RTLIL::Cell::setPort(RTLIL::IdString portname, RTLIL::SigSpec signal)
 	}
 
 
-	if (module->sig_norm_index != nullptr) {
+	if (module->sig_norm_index != nullptr && !ignored_cell(type)) {
 		module->sig_norm_index->dirty.insert(this);
 		if (!r.second) {
 			if (is_input_port) {
