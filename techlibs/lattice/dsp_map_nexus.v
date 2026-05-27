d6106f141cc66c116c79cdc85bdf16327c43bed8
nella <simontupy64@gmail.com>
2026-05-27 13:19:51 +0200

    Add matching for fused mac operations for Nexus (fix #5906).

--- techlibs/lattice/dsp_map_nexus.v
+++ techlibs/lattice/dsp_map_nexus.v
@@ -77,3 +77,92 @@ module \$__NX_MUL9X9 (input [8:0] A, input [8:0] B, output [17:0] Y);
 		.Z(Y)
 	);
 endmodule
+
+module \$__NX_MAC18X18 (A, B, C, Y);
+
+	parameter A_WIDTH = 18;
+	parameter B_WIDTH = 18;
+	parameter C_WIDTH = 48;
+	parameter Y_WIDTH = 48;
+	parameter A_SIGNED = 0;
+	parameter B_SIGNED = 0;
+	parameter SUBTRACT = 0;
+	input [17:0] A;
+	input [17:0] B;
+	input [47:0] C;
+	output [47:0] Y;
+	wire [53:0] Z_out;
+	assign Y = Z_out[47:0];
+
+	MULTADDSUB18X18 #(
+		.REGINPUTA("BYPASS"),
+		.REGINPUTB("BYPASS"),
+		.REGINPUTC("BYPASS"),
+		.REGOUTPUT("BYPASS")
+	) _TECHMAP_REPLACE_ (
+		.A(A), 
+		.B(B), 
+		.C({6'b0, C}),
+		.SIGNED(A_SIGNED ? 1'b1 : 1'b0), 
+		.ADDSUB(SUBTRACT ? 1'b1 : 1'b0),
+		.Z(Z_out)
+	);
+endmodule
+
+module \$__NX_PREADD18X18 (A, B, C, Y, CLK);
+
+	parameter PIPELINED = 0;
+	parameter A_SIGNED = 0;
+	parameter B_SIGNED = 0;
+	parameter C_SIGNED = 0;
+	input [17:0] A;
+	input [17:0] B;
+	input [17:0] C;
+	input CLK;
+	output [47:0] Y;
+	wire [35:0] Z_out;
+	assign Y = A_SIGNED ? {{12{Z_out[35]}}, Z_out} : {12'b0, Z_out};
+
+	MULTPREADD18X18 #(
+		.REGINPUTA("BYPASS"),
+		.REGINPUTB("BYPASS"),
+		.REGINPUTC("BYPASS"),
+		.REGOUTPUT(PIPELINED ? "REGISTER" : "BYPASS")
+	) _TECHMAP_REPLACE_ (
+		.A(A),
+		.B(B),
+		.C(C),
+		.CLK(CLK),
+		.SIGNEDA(A_SIGNED ? 1'b1 : 1'b0),
+		.SIGNEDB(B_SIGNED ? 1'b1 : 1'b0),
+		.SIGNEDC(C_SIGNED ? 1'b1 : 1'b0),
+		.Z(Z_out)
+	);
+endmodule
+
+module \$__NX_MAC9X9WIDE_4LANE (A0, B0, A1, B1, A2, B2, A3, B3, Y);
+
+	parameter SIGNED = 0;
+	input [8:0] A0, B0, A1, B1, A2, B2, A3, B3;
+	output [47:0] Y;
+	wire [53:0] Z_out;
+	assign Y = Z_out[47:0];
+
+	MULTADDSUB9X9WIDE #(
+		.REGINPUTAB0("BYPASS"),
+		.REGINPUTAB1("BYPASS"),
+		.REGINPUTAB2("BYPASS"),
+		.REGINPUTAB3("BYPASS"),
+		.REGINPUTC("BYPASS"),
+		.REGOUTPUT("BYPASS")
+	) _TECHMAP_REPLACE_ (
+		.A0(A0), .B0(B0),
+		.A1(A1), .B1(B1),
+		.A2(A2), .B2(B2),
+		.A3(A3), .B3(B3),
+		.C(54'b0),
+		.SIGNED(SIGNED ? 1'b1 : 1'b0),
+		.ADDSUB(4'b0000),
+		.Z(Z_out)
+	);
+endmodule
