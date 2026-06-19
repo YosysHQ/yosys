d974527ab4378b2df0194badb6a58c095d5f99c3
Emil J. Tywoniak <emil@tywoniak.eu>
2026-06-19 11:48:44 +0200

    rtlil_frontend: allocate meta block

--- frontends/rtlil/rtlil_frontend.cc
+++ frontends/rtlil/rtlil_frontend.cc
@@ -773,6 +773,8 @@ struct RTLILFrontendWorker {
 					} else
 						error("RTLIL error: redefinition of memory %s.", design->twines.str(mem_name).c_str());
 				}
+				if (memory->meta_ == nullptr)
+					memory->meta_ = design->alloc_obj_meta();
 				memory->meta_->name = mem_name;
 				break;
 			}
