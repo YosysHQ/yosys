450b5ae1b7549f3bbd6db8d4996e677917bd5457
N. Engelhardt <nak@yosyshq.com>
2026-09-23 16:04:42 +0200

    cmake: Fix Homebrew search on macOS

--- cmake/UseHomebrew.cmake
+++ cmake/UseHomebrew.cmake
@@ -3,7 +3,7 @@
 # 	use_homebrew([ROOT <root>])
 #
 # Includes all packages installed in `<root>` (output of brew --prefix if not
-# specified) in `CMAKE_FIND_ROOT_PATH`.
+# specified) in `CMAKE_PREFIX_PATH`.
 #
 function(use_homebrew)
 	cmake_parse_arguments(PARSE_ARGV 0 arg "" "ROOT" "")
@@ -29,9 +29,9 @@ function(use_homebrew)
 	file(GLOB package_roots ${arg_ROOT}/*/*) # e.g. `/opt/homebrew/Cellar/bison/3.8.2/`
 	foreach (package_root ${package_roots})
 		if (IS_DIRECTORY ${package_root})
-			list(APPEND CMAKE_FIND_ROOT_PATH ${package_root})
+			list(APPEND CMAKE_PREFIX_PATH "${package_root}")
 		endif()
 	endforeach()
 
-	return(PROPAGATE CMAKE_FIND_ROOT_PATH)
+	return(PROPAGATE CMAKE_PREFIX_PATH)
 endfunction()
